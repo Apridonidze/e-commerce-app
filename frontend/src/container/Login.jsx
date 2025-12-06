3953e6b6347a6656a1749366b1dbd50e14862858
unknown <giorgiapridonidze08@gmail.com>
2025-12-06 14:58:13 +0400

    feat : created form inputs for Login and Sign pages

--- frontend/src/container/Login.jsx
+++ frontend/src/container/Login.jsx
@@ -1,7 +1,17 @@
 const Login = () => {
     return(
         <div className="login-container">
-            Login.jsx
+             <form>
+                <div className="form-floating"> 
+                    <input className="form-control" type="text" id="email" placeholder="Email" />
+                    <label htmlFor="email">Email</label>
+                </div>
+                <div className="form-floating"> 
+                    <input className="form-control" type="text" id="name" placeholder="Full Name" />
+                    <label htmlFor="name">Password</label>
+                </div>
+                <input type="submit" value="Create Account" />
+            </form>
         </div>
     )
 }
--- frontend/src/container/Sign.jsx
+++ frontend/src/container/Sign.jsx
@@ -1,7 +1,29 @@
 const Sign = () => {
     return(
         <div className="sign-container">
-
+            <form>
+                <div className="form-floating"> 
+                    <input className="form-control" type="text" id="name" placeholder="Full Name" />
+                    <label htmlFor="name">Full Name</label>
+                </div>
+                <div className="form-floating"> 
+                    <input className="form-control" type="text" id="email" placeholder="Email" />
+                    <label htmlFor="email">Email</label>
+                </div>
+                <div className="form-floating"> 
+                    <input className="form-control" type="text" id="phonenumber" placeholder="Full Name" />
+                    <label htmlFor="phonenumber">Phone Number</label>
+                </div>
+                <div className="form-floating"> 
+                    <input className="form-control" type="text" id="name" placeholder="Full Name" />
+                    <label htmlFor="name">Password</label>
+                </div>
+                <div className="form-floating"> 
+                    <input className="form-control" type="text" id="name" placeholder="Full Name" />
+                    <label htmlFor="name">Confrim Password</label>
+                </div>
+                <input type="submit" value="Create Account" />
+            </form>
         </div>
     )
 }
