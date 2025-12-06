0c5bbb0e726c8e3b36bebd73bdfdbefd5e1e5632
unknown <giorgiapridonidze08@gmail.com>
2025-12-06 14:15:33 +0400

    feat : added categories and subcategories in Category.jsx

--- frontend/src/component/Category.jsx
+++ frontend/src/component/Category.jsx
@@ -1,7 +1,91 @@
 const Category = () => {
+
+    const categories = [
+        
+        {
+            "name": "Electronics",
+            "slug": "electronics",
+            "subcategories": [
+            "Smartphones & Accessories",
+            "Laptops & Computers",
+            "PC Parts & Components",
+            "Gaming Consoles",
+            "Audio & Headphones",
+            "Smart Home",
+            "Cameras & Drones"
+            ]
+        },
+        {
+            "name": "Home & Living",
+            "slug": "home-living",
+            "subcategories": [
+            "Furniture",
+            "Kitchen & Dining",
+            "Home Decor",
+            "Lighting",
+            "Cleaning Appliances"
+            ]
+        }, 
+        {
+            "name": "Fashion",
+            "slug": "fashion",
+            "subcategories": [
+            "Men's Clothing",
+            "Women's Clothing",
+            "Shoes",
+            "Accessories",
+            "Watches & Jewelry"
+            ]
+        },
+        {
+            "name": "Beauty & Personal Care",
+            "slug": "beauty",
+            "subcategories": [
+            "Skincare",
+            "Haircare",
+            "Makeup",
+            "Grooming Tools"
+            ]
+        },
+        {
+            "name": "Sports & Outdoors",
+            "slug": "sports-outdoors",
+            "subcategories": [
+            "Fitness Equipment",
+            "Outdoor Gear",
+            "Sportswear",
+            "Cycling Accessories"
+            ]
+        },
+        {
+            "name": "Automotive",
+            "slug": "automotive",
+            "subcategories": [
+            "Car Accessories",
+            "Auto Parts",
+            "Motorcycle Gear"
+            ]
+        },
+        {
+            "name": "Kids & Toys",
+            "slug": "kids-toys",
+            "subcategories": [
+            "Toys",
+            "Baby Essentials",
+            "Kids Clothing"
+            ]
+        }
+]
+
     return(
         <div className="category-container">
-            
+            {categories.map((cat , catId) => (
+                <select name={cat.name} id={catId}>
+                    {cat.subcategories.map((sub, subId) => (
+                        <option value={sub} key={sub}>{sub}</option>
+                    ))}
+                </select>
+            ))}
         </div>
     )
 }
--- frontend/src/container/Main.jsx
+++ frontend/src/container/Main.jsx
@@ -1,84 +1,10 @@
+import Category from "../component/Category"
 import Header from "../component/Header"
 import Sidebar from "../component/Sidebar"
 
 const Main = () => {
 
-    const categories = [
-        
-        {
-            "name": "Electronics",
-            "slug": "electronics",
-            "subcategories": [
-            "Smartphones & Accessories",
-            "Laptops & Computers",
-            "PC Parts & Components",
-            "Gaming Consoles",
-            "Audio & Headphones",
-            "Smart Home",
-            "Cameras & Drones"
-            ]
-        },
-        {
-            "name": "Home & Living",
-            "slug": "home-living",
-            "subcategories": [
-            "Furniture",
-            "Kitchen & Dining",
-            "Home Decor",
-            "Lighting",
-            "Cleaning Appliances"
-            ]
-        },
-        {
-            "name": "Fashion",
-            "slug": "fashion",
-            "subcategories": [
-            "Men's Clothing",
-            "Women's Clothing",
-            "Shoes",
-            "Accessories",
-            "Watches & Jewelry"
-            ]
-        },
-        {
-            "name": "Beauty & Personal Care",
-            "slug": "beauty",
-            "subcategories": [
-            "Skincare",
-            "Haircare",
-            "Makeup",
-            "Grooming Tools"
-            ]
-        },
-        {
-            "name": "Sports & Outdoors",
-            "slug": "sports-outdoors",
-            "subcategories": [
-            "Fitness Equipment",
-            "Outdoor Gear",
-            "Sportswear",
-            "Cycling Accessories"
-            ]
-        },
-        {
-            "name": "Automotive",
-            "slug": "automotive",
-            "subcategories": [
-            "Car Accessories",
-            "Auto Parts",
-            "Motorcycle Gear"
-            ]
-        },
-        {
-            "name": "Kids & Toys",
-            "slug": "kids-toys",
-            "subcategories": [
-            "Toys",
-            "Baby Essentials",
-            "Kids Clothing"
-            ]
-        }
-]
+    
 
 
     return(
@@ -88,6 +14,7 @@ const Main = () => {
             </div>
             <div className="main-end">
                 <Header />
+                <Category />
             </div>
         </div>
     )
