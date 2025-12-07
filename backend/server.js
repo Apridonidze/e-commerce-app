67400c4b2310324dcea3625eb11178323cc45b08
unknown <giorgiapridonidze08@gmail.com>
2025-12-07 14:25:46 +0400

    feat : added cors options to server.js

--- backend/server.js
+++ backend/server.js
@@ -3,10 +3,19 @@ const app = express()
 
 require('dotenv').config()
 
+const cors = require('cors')
+const CorsOptions = require('./config/CorsOptions')
+
+
+app.use(cors(CorsOptions))
+app.use(express.json())
+app.use(express.urlencoded({extended : true}))
+
 const PORT = process.env.PORT || 8080
 
 const LoginRouter = require('./routes/Login')
 const SignRouter = require('./routes/Sign')
+
 
 app.use('/login', LoginRouter)
 app.use('/sign', SignRouter)
