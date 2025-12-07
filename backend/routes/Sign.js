46b88cfbca9b78c6f7ebd912076a80a9ec37a467
unknown <giorgiapridonidze08@gmail.com>
2025-12-07 16:38:33 +0400

    feat : added error handling in Sign.js , to avoid duplicant users and returns errors in real time

--- backend/routes/Sign.js
+++ backend/routes/Sign.js
@@ -14,11 +14,28 @@ SignRouter.post('/' , async (req,res) => {
     try{
 
         const userinputs = req.body.data
-    
+
+        const [isFullnameInserted] = await db.query('select * from users where fullname = ? ' , [userinputs.name]);
+        const [emailInserted] = await db.query('select * from users where email = ? ' , [userinputs.email])
+        const [phoneNumberInserted] = await db.query('select * from users where country_code = ? and phone = ?' , [userinputs.phoneNumber.split(' ')[0],userinputs.phoneNumber.split(' ')[1]])
+        
+        let errorList = {}
+
+        if(isFullnameInserted.length > 0) errorList = {...errorList , state : 'name', nameError : 'User With This Name Already Exists'}
+        if(emailInserted.length > 0) errorList = {...errorList , state : 'email', nameError : 'This Email Is Already In Use'}
+        if(phoneNumberInserted.length > 0) errorList = {...errorList , state : 'phone' , nameError : 'This Phone Number Is Already Used'}
+
+        if(errorList.length > 0) return res.status(400).json(errorList)
+
         await db.query('INSERT INTO users (fullname, email, country_code, phone, password) VALUES (?, ?, ?, ?, ?)',[userinputs.name ,userinputs.email, userinputs.phoneNumber.split(' ')[0],userinputs.phoneNumber.split(' ')[1],userinputs.password]);
         return res.status(200).json('inserted')
         
     }catch(err){
+        if(err.code === 'ER_DUP_ENTRY'){
+            if(err.message.includes('users.fullname'))return res.status(400).json({errMessage : 'This Name Is Already In Use' , state : 'name' , err : err})
+            if(err.message.includes('users.email'))return res.status(400).json({errMessage : 'This Email Is Already In Use' , state : 'email' , err : err})
+        }
+
         return res.status(500).json({errMessage : 'Internal Error'  , err : err})
     }
 })
--- database/database.sql
+++ database/database.sql
@@ -5,4 +5,4 @@ use e_commerce_db
 
 /*create user tables*/
 
-create table users (id Int Primary key auto_increment , fullname varchar(55) unique , email varchar(55) unique , country_code varchar(5) , phone int(55) unique , password varchar(255))
+create table users (id Int Primary key auto_increment , fullname varchar(55) unique , email varchar(55) unique , country_code varchar(5) , phone int(55)  , password varchar(255))
