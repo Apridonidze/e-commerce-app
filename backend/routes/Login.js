80e97e28882918624d4df28ea4b31818198ba756
unknown <giorgiapridonidze08@gmail.com>
2025-12-07 16:49:22 +0400

    feat : added user inputs checking in Login.js apis to ensure user joins with valid email and password into account with error handlings

--- backend/routes/Login.js
+++ backend/routes/Login.js
@@ -1,9 +1,32 @@
 const express = require('express')
 const LoginRouter = express.Router()
 
+const db = require('../config/db')
 
-LoginRouter.post('/', (req,res) => {
-    console.log(req)
+const LoginSchema = require('../schemas/LoginSchema')
+
+LoginRouter.post('/', async (req,res) => {
+
+    const validateLogin = LoginSchema(req.body.data)
+
+    if(!validateLogin.success) return res.status(400).json({err : 'Invalid Inputs'})
+
+    try{
+        const userData = req.body.data
+
+        const [isEmailValid] = await db.query('select * from users where email = ?' , [userData.email]);
+
+        if(isEmailValid.length < 1) return res.status(400).json({err : 'Email Or Password Incorrect'})
+        
+        const [ isPasswordValid ] = await db.query('select * from users where email = ? and password = ?' , [userData.email , userData.password])
+
+        if(isPasswordValid.length < 1) return res.status(400).json({err : "Email Or Password Incorrect"})
+
+        return res.status(200).json({resp : 'User Found'})
+
+    }catch(err){
+        return res.status(500).json(err)
+    }
 })
 
 
