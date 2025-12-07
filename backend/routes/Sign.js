4059d3bd7a81983c73b705ee75bf6343c3fedc12
unknown <giorgiapridonidze08@gmail.com>
2025-12-07 16:22:02 +0400

    feat : added users insertion api in Sign.js

--- backend/routes/Sign.js
+++ backend/routes/Sign.js
@@ -3,7 +3,9 @@ const SignRouter = express.Router()
 
 const SignSchema = require('../schemas/SignSchema')
 
-SignRouter.post('/' , (req,res) => {
+const db = require('../config/db')
+
+SignRouter.post('/' , async (req,res) => {
     
     const inputValidateResp = SignSchema(req.body.data)
 
@@ -11,7 +13,10 @@ SignRouter.post('/' , (req,res) => {
         
     try{
 
-
+        const userinputs = req.body.data
+    
+        await db.query('INSERT INTO users (fullname, email, country_code, phone, password) VALUES (?, ?, ?, ?, ?)',[userinputs.name ,userinputs.email, userinputs.phoneNumber.split(' ')[0],userinputs.phoneNumber.split(' ')[1],userinputs.password]);
+        return res.status(200).json('inserted')
         
     }catch(err){
         return res.status(500).json({errMessage : 'Internal Error'  , err : err})
