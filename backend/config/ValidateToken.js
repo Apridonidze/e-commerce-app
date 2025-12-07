393e1ed65545fd62c4553b1598282f77a7293873
unknown <giorgiapridonidze08@gmail.com>
2025-12-08 03:01:59 +0400

    feat : created ValidateToken.js middleware in config folder to validate users tokens after every actions that needs user to be authorized

---
+++ backend/config/ValidateToken.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken')
+
+require('dotenv').config()
+
+function ValidateToken ( req , res , next ) {
+
+    const AuthHeaders = req.headers['authorization']
+
+    if(!AuthHeaders || !AuthHeaders.split(' ').length < 1) return res.status(400).json({err : 'Invalid Headers'})
+
+    const token = AuthHeaders.split(' ')[1]
+    
+    try{
+
+        const isValid = jwt.verify(token , process.env.JWT_SECRET_KEY)
+
+        req.user = isValid
+
+        next()
+
+    }catch(err){
+        return res.status(401).json({errMessage : 'Invalid Token', err : err})
+    }
+
+}
+
+module.exports = ValidateToken
