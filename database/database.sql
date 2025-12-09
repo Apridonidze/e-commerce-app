4652c439d21db2eefe17e7520e0f6f112d91ebcd
unknown <giorgiapridonidze08@gmail.com>
2025-12-09 22:27:40 +0400

    feat : created table to save saved products

--- database/database.sql
+++ database/database.sql
@@ -12,3 +12,7 @@ create table users (id Int Primary key auto_increment , fullname varchar(55) uni
 
 
 create table products (id int , product_id int primary key auto_increment  , images JSON, title varchar(55), description varchar(500) , category varchar(55) , subcategory varchar(55))
+
+
+/* creates saved products table */
+create table saved_products (id int , product_id int)
