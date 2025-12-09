8f0b62c34c601756dcd1b813d1e9c17604b9aa16
unknown <giorgiapridonidze08@gmail.com>
2025-12-09 17:06:44 +0400

    feat : added products inputs validation in backend products.js with zod schema

--- backend/routes/Products.js
+++ backend/routes/Products.js
@@ -13,16 +13,20 @@ const db = require('../config/db')
 ProductsRouter.post('/' , ValidateToken, upload.fields([{name :'images', maxCount : 20}]), async (req,res) => {
 
     const validateProduct = NewProductSchema(req.body)
-
+    
 
     if(!validateProduct.success) return res.status(400).json({errMessage : 'Invalid Input'})
 
     try{
+        const product = req.body
         
         const files = req.files?.images;
         const filesBuffer = await Promise.all(files.map(file => fs.promises.readFile(file.path)));
         const base64 = filesBuffer.map(buffer => buffer.toString("base64"))
 
+
+        await db.query('insert into products (id, images, title, description , category , subcategory) values (?,?,?,?,?,?)' , [req.user.userId , [JSON.stringify(base64)] , product.name , product.description , product.category , product.subCategory])
+        return res.status(200).json({message : 'product added succsefully' , productDetails : `${product.name}${product.description}${product.category}${product.subCategory}`})
 
     }catch(err){
         return res.status(500).json({errMessage : 'Internal Error'  , err : err})
