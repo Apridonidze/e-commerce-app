3cea71716d2c7334a46abda66eb90c9cc386f7c1
unknown <giorgiapridonidze08@gmail.com>
2025-12-09 19:49:44 +0400

    feat : designed user route , so now it returns users nonsensitive data from database and returns response to frontend, (if user is not authorized and still manages to go to this page it returns 400 status code)

--- backend/routes/Users.js
+++ backend/routes/Users.js
@@ -3,11 +3,19 @@ const UsersRouter = express.Router()
 
 const ValidateToken = require('../config/ValidateToken')
 
-UsersRouter.get('/', ValidateToken ,(req,res) => {
+const db = require('../config/db')
+
+UsersRouter.get('/', ValidateToken , async(req,res) => {
     try{
 
+        const id = req.user.userId
 
+        const [ user ] = await db.query('select fullname, email,country_code, phone  from users where id = ?' , id)
         
+        if(user.length < 1) return res.status(400).json({errMessage : "User Not Found" , user : null})
+
+        return res.status(200).json({message : 'User Found' , user : user[0]})
+
 
     }catch(err){
         return res.status(500).json({errMessage : 'Internal Error'  , err : err})
