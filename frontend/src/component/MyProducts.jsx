bb688930136c1e2ff6e0daf652e4addfb6fd1984
unknown <giorgiapridonidze08@gmail.com>
2025-12-10 14:18:21 +0400

    feat : created basic layoput for myproducts.jsx with add new product button

--- frontend/src/component/MyProducts.jsx
+++ frontend/src/component/MyProducts.jsx
@@ -3,7 +3,12 @@ import Product from "./Product";
 const MyProducts = ( { products } ) => {
     
     return(
-        <div className="my-products-container">
+        <div className="my-products-container col border border-2 my-3">
+            <div className="my-products-header position-relative d-flex justify-content-between" style={{bottom: '15px'}}>
+                    
+                <span className=' bg-white' >{'Your Products'|| <Skeleton />}</span> 
+                <button className="btn btn-sm btn-success">Add New Product</button>
+            </div>
             {products.products?.map((prod , prodId) => (
                 <Product prod={prod} prodId={prodId} key={prodId} user={products.user}/>
             ))}
