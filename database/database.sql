409dc0837dab2749703ce6a10cf85ce98843f6fe
unknown <giorgiapridonidze08@gmail.com>
2025-12-13 03:41:07 +0400

    fix : fixed database tables in database.sql to use foreign and primary keys for joins

--- database/database.sql
+++ database/database.sql
@@ -8,6 +8,6 @@ use e_commerce_db;
 -- create users table --
 create table users (id Int Primary key auto_increment , fullname varchar(55) unique , email varchar(55) unique , country_code varchar(5) , phone int(55)  , password varchar(255));
 -- create products table --
-create table products (id int, products_id int auto_increment primary key ,  creator_id int,images JSON , title varchar(55), description varchar(5000),category varchar(55), subcategory varchar(55), price int, foreign key (creator_id) references users(id));
+create table products (id int, products_id int auto_increment primary key ,images JSON , title varchar(55), description varchar(5000),category varchar(55), subcategory varchar(55), price int, foreign key (id) references users(id));
 -- create saved products table --
-create table saved_products (id int, product_id int, creator_id int , foreign key (id) references users(id) , foreign key (product_id) references products(products_id) ,foreign key (creator_id) references products(creator_id));
+create table saved_products (id int, product_id int, creator_id int , foreign key (id) references users(id) , foreign key (product_id) references products(products_id) ,foreign key (creator_id) references products(id));
