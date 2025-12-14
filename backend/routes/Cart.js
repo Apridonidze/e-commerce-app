d500a3561b1a384271d11cf73d4165a032ca31b0
unknown <giorgiapridonidze08@gmail.com>
2025-12-14 23:26:54 +0400

    feat : developed cart get api endpoint so users can retrieve products that are in cart

--- backend/routes/Cart.js
+++ backend/routes/Cart.js
@@ -2,10 +2,20 @@ const express = require('express');
 const CartRouter = express.Router();
 
 const ValidateToken = require('../config/ValidateToken');
-
+const db = require('../config/db')
 
 CartRouter.get('/' , ValidateToken , async(req,res) => {
-    
+    try{
+        
+        const [ cartItems ] = await db.query('select * from cart where id = ?' , req.user.userId)
+
+        if(cartItems.length < 1) return res.status(400).json({message : 'No Items In Cart', products : []})
+        
+        return res.status(200).json({message : 'Found Items In Your Cart' , products : cartItems})
+
+    }catch(err){
+        return res.status(500).json({errMessage : 'Internal Error' , err : err})
+    }
 })
 
 CartRouter.post('/' , ValidateToken , async (req, res) => {
