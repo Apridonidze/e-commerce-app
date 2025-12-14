e5a51ed7b6c42e2f78c171fe279faa0e1d760291
unknown <giorgiapridonidze08@gmail.com>
2025-12-14 23:33:21 +0400

    feat : developed cart post request that inserts product into cart table with id checking and product checking

--- backend/routes/Cart.js
+++ backend/routes/Cart.js
@@ -18,8 +18,27 @@ CartRouter.get('/' , ValidateToken , async(req,res) => {
     }
 })
 
-CartRouter.post('/' , ValidateToken , async (req, res) => {
-    res.send('Cart Post Api Endpoint')
+CartRouter.post('/:id' , ValidateToken , async (req, res) => {
+    
+    try{
+
+        const productId = req.params.id
+
+        if(!Number(productId)) return res.status(400).json({message : "Invalid Product Id"})
+
+        const [ doesProductExists ] = await db.query('select * from products where products_id = ?' , productId)
+        if(doesProductExists.length < 1) return res.status(400).json({message : 'Product Does Not Exists In Database'})
+
+        const [ isAlreadyInCart ] = await db.query('select * from cart where id = ? and product_id = ?' , [req.user.userId , productId])
+        if(isAlreadyInCart.length > 0) return res.status(400).json({message : "You Already Have This Item In Your Cart"})
+        
+        await db.query('insert into cart (id, product_id) values (?,?)' , [req.user.userId , productId])
+        return res.status(200).json({message : "Prodcut Added In Cart Successfully"})
+
+    }catch(err){
+        return res.status(500).json({errMessage : "Internal Error" , err : err})
+    }
+
 })
 
 module.exports = CartRouter
