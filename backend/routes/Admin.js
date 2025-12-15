d42740934bfc6e245a3da53a0eefee9d5ea07764
unknown <giorgiapridonidze08@gmail.com>
2025-12-15 12:31:28 +0400

    feat : developed admin post  api endpoint to give admin access to add another admin in db

--- backend/routes/Admin.js
+++ backend/routes/Admin.js
@@ -9,6 +9,8 @@ AdminRouter.get('/' , ValidateToken , async(req,res) => {
 
         const id = req.user.userId
 
+        // const [ doesUserExists ]
+
         const [ isAdmin ] = await db.query('select id from admin where id = ?' , id)
         if(isAdmin.length < 1) return res.status(400).json({message : "Access Declined"})
         
@@ -19,8 +21,20 @@ AdminRouter.get('/' , ValidateToken , async(req,res) => {
     }
 })
 
-AdminRouter.post('/add-new-admin' , ValidateToken , async(req,res) => {
-    res.send('Post API Endpoint Route for Admin')
+AdminRouter.post('/add-new-admin/:id' , ValidateToken , async(req,res) => {
+    try{
+        const id = req.user.userId
+        const newAdmId = req.params.id
+
+        const [ isAdmin ] = await db.query('select id from admin where id = ?' , id)
+        if(isAdmin.length < 1) return res.status(400).json({message : "Access Declined"})
+        
+        await db.query('insert into admin (id) values (?)' , newAdmId)
+        return res.status(200).json({message : 'New Admin Successfully Added To List' , newAdminId : newAdmId})
+
+    }catch(err){
+        return res.status(500).json({errMessage : "Internal Error" , err : err})
+    }
 })
 
 module.exports = AdminRouter
