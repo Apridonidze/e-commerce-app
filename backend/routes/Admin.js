bf6ad2055be5ca4f9711eb7ebc97613961255298
unknown <giorgiapridonidze08@gmail.com>
2025-12-15 12:22:19 +0400

    feat : developed admin get api endpoint to check if user is admin before accessing admin features or dashboard

--- backend/routes/Admin.js
+++ backend/routes/Admin.js
@@ -2,9 +2,21 @@ const express = require('express')
 const AdminRouter = express.Router()
 
 const ValidateToken = require('../config/ValidateToken')
+const db = require('../config/db')
 
 AdminRouter.get('/' , ValidateToken , async(req,res) => {
-    res.send('Get API Endpoint Route for Admin')
+    try{
+
+        const id = req.user.userId
+
+        const [ isAdmin ] = await db.query('select id from admin where id = ?' , id)
+        if(isAdmin.length < 1) return res.status(400).json({message : "Access Declined"})
+        
+        return res.status(200).json({message : 'Access Granted', isAdmin : true})
+
+    }catch(err){
+        return res.status(500).json({errMessage : "Internal Error" , err : err})
+    }
 })
 
 AdminRouter.post('/add-new-admin' , ValidateToken , async(req,res) => {
