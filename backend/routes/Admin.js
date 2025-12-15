aa360a69d68e57268fd819a2794cb0127506ee13
unknown <giorgiapridonidze08@gmail.com>
2025-12-15 16:48:11 +0400

    feat : created /add-new-admin/ api delete endpoint to give admin access to delete other admins

--- backend/routes/Admin.js
+++ backend/routes/Admin.js
@@ -46,4 +46,29 @@ AdminRouter.post('/add-new-admin/:id' , ValidateToken , async(req,res) => {
     }
 })
 
+
+AdminRouter.delete('/add-new-admin/:id' , ValidateToken , async(req,res) => {
+    try{
+        const id = req.user.userId
+        const admId = req.params.id
+
+        
+        const [ doesUserExists ] = await db.query('select id from users where id = ?' , id)
+        if(doesUserExists.length < 1) return res.status(400).json({message : "User Not Found"})
+
+        
+        const [ doesNewAdminExists ] = await db.query('select id from users where id = ?' , admId)
+        if(doesNewAdminExists.length < 1) return res.status(400).json({message : "User Not Found"})
+
+        const [ isAdmin ] = await db.query('select id from admin where id = ?' , id)
+        if(isAdmin.length < 1) return res.status(400).json({message : "Access Declined"})
+        
+        await db.query('delete from admin where id = ?' , admId)
+        return res.status(200).json({message : 'New Admin Successfully Added To List' , admId : admId})
+
+    }catch(err){
+        return res.status(500).json({errMessage : "Internal Error" , err : err})
+    }
+})
+
 module.exports = AdminRouter
