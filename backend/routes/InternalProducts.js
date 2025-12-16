3004997da4b6fbf7ba26556019877d35be662443
unknown <giorgiapridonidze08@gmail.com>
2025-12-17 03:12:54 +0400

    feat : created on-way-items products api endpoint ot display items that are on way to customers

--- backend/routes/InternalProducts.js
+++ backend/routes/InternalProducts.js
@@ -5,7 +5,7 @@ const db = require('../config/db')
 const isAdmin = require('../config/isAdmin')
 const ValidateToken = require('../config/ValidateToken')
 
-InternalProducts.get('/' ,ValidateToken, isAdmin , async(req,res) => {
+InternalProducts.get('/pending-items' ,ValidateToken, isAdmin , async(req,res) => {
     try{
     
         const [ PendingProducts ] = await db.query('select products.* , cart.* , users.id , users.fullname , users.email from cart join products on cart.product_id = products.products_id join users on products.id = users.id where cart.status = ?' , 'pending')
@@ -16,6 +16,19 @@ InternalProducts.get('/' ,ValidateToken, isAdmin , async(req,res) => {
 
     }catch(err){
         return res.status(500).json({errMessage : 'Internal Error' , err : err})
+    }
+})
+
+InternalProducts.get('/on-way-items' ,ValidateToken, isAdmin , async(req,res) => {
+    try{
+        
+        const [ PendingProducts ] = await db.query('select products.* , cart.* , users.id , users.fullname , users.email from cart join products on cart.product_id = products.products_id join users on products.id = users.id where cart.status = ?' , 'on way')
+        if(PendingProducts.length < 1) return res.status(400).json({message : "No Products Beign Ordered" , PendingProducts : []})
+
+        return res.status(200).json({message : "Products On Way" , OnWayProducts: PendingProducts})
+
+    }catch(err){
+        return res.status(500).json({errMessage : "Internal Error" , err : err})
     }
 })
 
