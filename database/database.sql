620dc02c5064cb1713b2ec933556a6bf1f6622b2
unknown <giorgiapridonidze08@gmail.com>
2025-12-16 14:32:44 +0400

    mod : added amount integer to products table

--- database/database.sql
+++ database/database.sql
@@ -8,7 +8,7 @@ use e_commerce_db;
 -- create users table --
 create table users (id Int Primary key auto_increment , fullname varchar(55) unique , email varchar(55) unique , country_code varchar(5) , phone int(55)  , password varchar(255));
 -- create products table --
-create table products (id int, products_id int auto_increment primary key ,images JSON , title varchar(55), description varchar(5000),category varchar(55), subcategory varchar(55), price decimal(12,2), foreign key (id) references users(id));
+create table products (id int, products_id int auto_increment primary key ,images JSON , title varchar(55), description varchar(5000),category varchar(55), subcategory varchar(55), price decimal(12,2), amount int, foreign key (id) references users(id));
 -- create saved products table --
 create table saved_products (id int, product_id int, foreign key (id) references users(id) , foreign key (product_id) references products(products_id));
 -- create table for admin users --
