330211b30551601258f02d4aae84b8f300bc47dd
unknown <giorgiapridonidze08@gmail.com>
2025-12-16 22:42:59 +0400

    mod : implemented isAdmin middleware to admin.js apis

--- backend/routes/Admin.js
+++ backend/routes/Admin.js
@@ -17,21 +17,8 @@ AdminRouter.get('/' , ValidateToken , isAdmin ,  async(req,res) => {
 
 
 
-AdminRouter.post('/add-new-admin/:id' , ValidateToken , async(req,res) => {
+AdminRouter.post('/add-new-admin/:id' , ValidateToken , isAdmin , async(req,res) => {
     try{
-        const id = req.user.userId
-        const newAdmId = req.params.id
-
-        
-        const [ doesUserExists ] = await db.query('select id from users where id = ?' , id)
-        if(doesUserExists.length < 1) return res.status(400).json({message : "User Not Found"})
-
-        
-        const [ doesNewAdminExists ] = await db.query('select id from users where id = ?' , newAdmId)
-        if(doesNewAdminExists.length < 1) return res.status(400).json({message : "User Not Found"})
-
-        const [ isAdmin ] = await db.query('select id from admin where id = ?' , id)
-        if(isAdmin.length < 1) return res.status(400).json({message : "Access Declined"})
         
         await db.query('insert into admin (id) values (?)' , newAdmId)
         return res.status(200).json({message : 'New Admin Successfully Added To List' , newAdminId : newAdmId})
@@ -42,21 +29,10 @@ AdminRouter.post('/add-new-admin/:id' , ValidateToken , async(req,res) => {
 })
 
 
-AdminRouter.delete('/add-new-admin/:id' , ValidateToken , async(req,res) => {
+AdminRouter.delete('/add-new-admin/:id' , ValidateToken , isAdmin , async(req,res) => {
     try{
-        const id = req.user.userId
         const admId = req.params.id
 
-        
-        const [ doesUserExists ] = await db.query('select id from users where id = ?' , id)
-        if(doesUserExists.length < 1) return res.status(400).json({message : "User Not Found"})
-
-        
-        const [ doesNewAdminExists ] = await db.query('select id from users where id = ?' , admId)
-        if(doesNewAdminExists.length < 1) return res.status(400).json({message : "User Not Found"})
-
-        const [ isAdmin ] = await db.query('select id from admin where id = ?' , id)
-        if(isAdmin.length < 1) return res.status(400).json({message : "Access Declined"})
         
         await db.query('delete from admin where id = ?' , admId)
         return res.status(200).json({message : 'New Admin Successfully Added To List' , admId : admId})
