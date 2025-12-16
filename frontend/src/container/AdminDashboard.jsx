1a04c0cc26cf0a72bd43c558cc307ae5e3055be5
unknown <giorgiapridonidze08@gmail.com>
2025-12-16 20:05:51 +0400

    feat : added second admin role checking in AdminDashboard.jsx

--- frontend/src/container/AdminDashboard.jsx
+++ frontend/src/container/AdminDashboard.jsx
@@ -1,4 +1,31 @@
+import axios from "axios"
+import { useEffect, useState } from "react"
+import { BACKEND_URL } from "../../config"
+import { useCookies } from "react-cookie"
+
 const AdminDashboard = () => {
+
+    
+    const [ cookies ] = useCookies(['token'])
+    const [ isAdmin, setIsAdmin ] = useState(false)
+
+    useEffect(() => {
+
+        const fetchStatus = async() => {
+
+            try{
+
+                await axios.get(`${BACKEND_URL}/admin` , {headers : {Authorization : `Bearer ${cookies.token}`}}).then(resp => {console.log(resp) ; setIsAdmin(resp.data.isAdmin)})
+
+            }catch(err){
+                console.log(err)
+            }
+
+        }
+        
+        return () => {fetchStatus()};
+
+    },[])
 
     return(
         <div className="admin-dashboard-container">
