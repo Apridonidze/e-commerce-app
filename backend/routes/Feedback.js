bbf1e26508b4f4378c9b17767d57fe3058663953
unknown <giorgiapridonidze08@gmail.com>
2025-12-17 14:21:41 +0400

    feat : developed feedbacks get api endpoint in Feedback.js to get all feedbacks for admin dashboard

--- backend/routes/Feedback.js
+++ backend/routes/Feedback.js
@@ -1,5 +1,21 @@
 const express = require('express')
 const FeedbackRouter = express.Router()
 
+const ValidateToken = require('../config/ValidateToken')
+const isAdmin = require('../config/isAdmin')
+const db = require('../config/db')
+
+FeedbackRouter.get('/' , ValidateToken, isAdmin , async(req,res) => {
+    try{
+
+        const [ feedbacks ] = await db.query('select feedback.* users.fullname, users.id from feedback join users on users.id = feedback.id')
+        if(feedbacks.length < 1) return res.status(400).json({message : "No Feedbacks Yet." , feedbacks : feedbacks})
+
+        return res.status(200).json({message : "Feedbacks Found" , feedbacks})
+
+    }catch(err){
+        return res.status(500).json({errMessage : "Internal Error" , err : err})
+    }
+})
 
 module.exports = FeedbackRouter
