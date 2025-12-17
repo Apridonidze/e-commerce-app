0192497f9229a518055dd149834c9adeef42d05d
unknown <giorgiapridonidze08@gmail.com>
2025-12-17 04:26:33 +0400

    feat : created table for reports in database.sql

--- backend/routes/Reports.js
+++ backend/routes/Reports.js
@@ -6,8 +6,23 @@ const ValidateToken = require('../config/ValidateToken')
 const isAdmin = require('../config/isAdmin')
 
 ReportsRouter.get('/product-reports', ValidateToken, isAdmin, async(req,res) => {
-    
+    try{
+
+
+
+    }catch(err){
+        return res.status(500).json({errMessage : "Internal Erorr" , err : err})
+    }
 })
 
+ReportsRouter.get('/report-product/product_id=:id', ValidateToken, async(req,res) => {
+    try{
+
+        
+
+    }catch(err){
+        return res.status(500).json({errMessage : "Internal Erorr" , err : err})
+    }
+})
 
 module.exports = ReportsRouter
--- database/database.sql
+++ database/database.sql
@@ -17,3 +17,5 @@ create table admin (id int , foreign key (id) references users(id));
 create table cart (id int, product_id int , status enum("none" , "pending" , 'on way', "delivered") ,foreign key (id) references users(id) , foreign key (product_id) references products(products_id));
 -- create table for bought items to display in users dashboard later---
 create table bought_products (id int , product_id int , foreign key (id) references users(id) , foreign key (product_id) references products(products_id));
+-- create table for reports  --
+create table reports (id int , report_id int primary key, type enum("product", "webapp" , "delivery"), content varchar(255), foreign key reports(id) references users(id))
