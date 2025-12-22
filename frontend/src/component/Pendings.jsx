ff2f15a918167589bc747332ec08d3d7125a2728
unknown <giorgiapridonidze08@gmail.com>
2025-12-22 15:33:24 +0400

    feat : created ProductLine.jsx to display products in AdminDashboard.jsx (it will have different ddesing from Product.js , so it will be easier to display prodcut on admindashboard)

--- frontend/src/component/Pendings.jsx
+++ frontend/src/component/Pendings.jsx
@@ -1,7 +1,12 @@
-const Pendings = () => {
+import Product from "./Product"
+
+const Pendings = ({ pendings }) => {
     return(
         <div className="pendings-container">
-            Pendings
+            <h1>Pending Products</h1>
+            {pendings?.map((prod, prodId) => (
+                <Product prod={prod} prodId={prodId} key={prodId}/>
+            ))}
         </div>
     )
 }
---
+++ frontend/src/component/ProductLine.jsx
@@ -0,0 +1,7 @@
+const ProductLine = () => {
+    return(
+        <div className="product-line-container"></div>
+    )
+}
+
+export default ProductLine
