79c65496102fbbafba07fbc392684f4128635c5e
unknown <giorgiapridonidze08@gmail.com>
2025-12-22 15:47:13 +0400

    feat: added content into ProductLine.jsx , now component displays item name , user who ordered item , when it was ordered and what status it has , with button to change oders status

--- frontend/src/component/Pendings.jsx
+++ frontend/src/component/Pendings.jsx
@@ -1,11 +1,11 @@
-import Product from "./Product"
+import ProductLine from "./ProductLine"
 
 const Pendings = ({ pendings }) => {
     return(
         <div className="pendings-container">
             <h1>Pending Products</h1>
             {pendings?.map((prod, prodId) => (
-                <Product prod={prod} prodId={prodId} key={prodId}/>
+                <ProductLine prod={prod} prodId={prodId} key={prodId}/>
             ))}
         </div>
     )
--- frontend/src/component/ProductLine.jsx
+++ frontend/src/component/ProductLine.jsx
@@ -1,6 +1,17 @@
-const ProductLine = () => {
+import { Link } from "react-router-dom"
+
+const ProductLine = ({ prod, prodId, key }) => {
     return(
-        <div className="product-line-container"></div>
+        <div className="product-line-container border my-2 d-flex justify-content-between" key={key}>
+            <div className="product-start">
+                <Link >{prod.title}</Link> <span>Ordered By : {prod.fullname}</span> <span>Status : {prod.status} Ordered In : {prod.date}</span>
+            </div> {/** add link to product */}
+
+            <div className="product-end">
+                {prod.status == 'pending' || prod.status == 'on way' ? 
+                <button>Change Status</button> : <></>}
+            </div>
+        </div>
     )
 }
 
