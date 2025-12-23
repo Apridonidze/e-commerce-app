3f9a0d9b08470ed95de837b0b1c32b5e2cc340c2
unknown <giorgiapridonidze08@gmail.com>
2025-12-23 14:19:44 +0400

    mod : modified isAdmin middleware in backend with status codes

--- backend/config/isAdmin.js
+++ backend/config/isAdmin.js
@@ -6,10 +6,10 @@ async function isAdmin ( req , res , next) {
         const id = req.user.userId
 
         const [ doesUserExists ] = await db.query('select id from users where id = ?' , id)
-        if(doesUserExists.length < 1) return res.status(400).json({message : "User Not Found" , isAdmin : false})
+        if(doesUserExists.length < 1) return res.status(404).json({message : "User Not Found" , isAdmin : false})
 
         const [ isAdmin ] = await db.query('select id from admin where id = ?' , id)
-        if(isAdmin.length < 1) return res.status(400).json({message : "Access Declined" , isAdmin : false})
+        if(isAdmin.length < 1) return res.status(403).json({message : "Access Declined" , isAdmin : false})
 
         next();
 
