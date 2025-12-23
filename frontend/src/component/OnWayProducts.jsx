8604aae1b74bc00c6a1d0365825813c7662a6bcc
unknown <giorgiapridonidze08@gmail.com>
2025-12-23 14:07:03 +0400

    feat: dispalyed on way products in OnWayProducts.jsx component

--- frontend/src/component/OnWayProducts.jsx
+++ frontend/src/component/OnWayProducts.jsx
@@ -1,7 +1,11 @@
-const OnWayProducts = () => {
+import Skeleton from "react-loading-skeleton"
+import ProductLine from "./ProductLine"
+
+const OnWayProducts = ({ onway }) => {
     return(
         <div className="on-way-products-container">
-            On Way Products
+            <h1>On Way Products</h1>
+            {onway?.map((prod, prodId) => <ProductLine prod={prod} prodId={prodId} key={prodId}/>) || <Skeleton count={3}/>}
         </div>
     )
 }
--- frontend/src/component/Pendings.jsx
+++ frontend/src/component/Pendings.jsx
@@ -1,12 +1,12 @@
+import Skeleton from "react-loading-skeleton"
+
 import ProductLine from "./ProductLine"
 
 const Pendings = ({ pendings }) => {
     return(
         <div className="pendings-container">
             <h1>Pending Products</h1>
-            {pendings?.map((prod, prodId) => (
-                <ProductLine prod={prod} prodId={prodId} key={prodId}/>
-            ))}
+            {pendings?.map((prod , prodId) => <ProductLine prod={prod} prodId={prodId} key={prodId}/>) || <Skeleton count={3}/>}
         </div>
     )
 }
