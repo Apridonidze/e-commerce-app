ab108de8c883e58a6ea8982089bf77960bac891d
unknown <giorgiapridonidze08@gmail.com>
2025-12-23 15:27:21 +0400

    feat : added conetent in Products.jsx to diplay latest products with admin names who added product

--- frontend/src/component/Products.jsx
+++ frontend/src/component/Products.jsx
@@ -1,7 +1,11 @@
+import Skeleton from "react-loading-skeleton"
+import ProductLine from "./ProductLine"
+
 const Products = ({ latestProducts }) => {
     return(
         <div className="products-container">
             <h1>latest products</h1>
+            {latestProducts?.map((prod, prodId) => <ProductLine prod={prod} prodId={prodId} key={prodId}/>) || <Skeleton count={3}/>}
         </div>
     )
 }
