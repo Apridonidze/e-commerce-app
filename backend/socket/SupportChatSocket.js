aba25634bf1d3df555eb77e1b9969dab9a3f4ba0
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-01-15 17:04:03 +0400

    feat: added admins joining feat to ddisplay if any admins are online for clients

--- backend/socket/SupportChatSocket.js
+++ backend/socket/SupportChatSocket.js
@@ -16,39 +16,53 @@ function SupportChatSocket (server) {
     const wss = new WebSocketServer({ server })
     
     wss.on("connection" , async(ws, req) => {
+
         console.log('SupportChatSocket initialized');
+
         const query = url.parse(req.url, true).query;
         const token = query.token;
 
         const validatedUser = ValidateSocketToken(token , ws)
         if(!validatedUser) return;
         
-        
+        const isAdmin = null;
+
+        try{
+            const [ AdminList ] = await db.query('select * from admin join users on users.id = admin.id where id = ?',[ws.user.userId])
+            if(AdminList.length < 1) isAdmin = false;
+            isAdmin = true
+
+        }catch(err){
+            ws.send(JSON.stringify({type : 'internal_error' , message : err}))
+        }
+                        
         //check users role , if its admin add them to adminlist and send to frotnedn , else return (avoid duplicates)
         //if user is admin do not assign convid to them
         
-        try{
+        if(isAdmin == false){
+            try{
 
-            const [ convId ] = await db.query('select support_messages.conversation_id, support_messages.sender_id from support_messages join users on support_messages.sender_id = users.id where sender_id = ? ' , [ws.user.userId])
-            
-            if(convId.length > 0){
-                ws.convId = uuid(2)
-            }; 
-            ws.convId = convId[0].conversation_id
-            const [prevMessages] = await db.query('select support_messages.sender_id , support_messages.content, support_messages.created_at from support_messages join users on support_messages.sender_id = users.id where support_messages.conversation_id  = ? ORDER BY support_messages.message_id DESC LIMIT 15' , [ws.convId])
+                console.log('you are admin')
+                const [ convId ] = await db.query('select support_messages.conversation_id, support_messages.sender_id from support_messages join users on support_messages.sender_id = users.id where sender_id = ? ' , [ws.user.userId])
+                
+                if(convId.length > 0){
+                    ws.convId = uuid(2)
+                }; 
+                
+                ws.convId = convId[0].conversation_id
+                const [prevMessages] = await db.query('select support_messages.sender_id , support_messages.content, support_messages.created_at from support_messages join users on support_messages.sender_id = users.id where support_messages.conversation_id  = ? ORDER BY support_messages.message_id DESC LIMIT 15' , [ws.convId])
 
-            ws.send(JSON.stringify({type: 'recieve_convid' , convId : ws.convId}))
-            ws.send(JSON.stringify({type : "recieve_support_chat_message" , message : prevMessages}))//add who is sender (you or other)
-            
-            if(!rooms.some(CI => CI === ws.convId)) rooms.push(ws.convId)
-            
-
-        }catch(err){
-            ws.send(JSON.stringify({type: 'internal_error' , message : err}))
+                ws.send(JSON.stringify({type: 'recieve_convid' , convId : ws.convId}))
+                ws.send(JSON.stringify({type : "recieve_support_chat_message" , message : prevMessages}))//add who is sender (you or other)
+                
+                if(!rooms.some(CI => CI === ws.convId)) rooms.push(ws.convId)
+                
+            }catch(err){
+                ws.send(JSON.stringify({type: 'internal_error' , message : err}))
+            }
         }
-        
-        console.log(rooms)
 
+        
         ws.on('message' , async(data) => {
 
             //validate message
