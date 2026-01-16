0822e8a353cd69cfc6f3e3eaac0bb236428f3f62
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-01-16 17:51:47 +0400

    feat: develop[ed ValidateSocketAdmin middleware and tested it locally (works perfectly) + added status  send to frontend

--- backend/socket.config/ValidateSocketAdmin.js
+++ backend/socket.config/ValidateSocketAdmin.js
@@ -1,10 +1,19 @@
-function ValidateSocketAdmin (user, ws) {
+const db = require('../config/db')
+
+async function ValidateSocketAdmin (user, ws) {
     try{
 
-        console.log(user)
+        const [isAdmin] = await db.query('select * from admin where id = ?',[user.userId])  
+        if(isAdmin.length === 0) {ws.send(JSON.stringify({type : "admin_access" , status : 403 , admin_access : false})) ; return false};
+        
+        ws.send(JSON.stringify({type : "admin_access" , status : 200 , admin_access : true}))
+        return true;
 
     }catch(err){
-        console.log(err)
+        ws.send(JSON.stringify({type : "admin_access" , status : 500 , admin_access : false}))
+        ws.send(JSON.stringify({type : "internal_error" , message : err}))
+        ws.close()
+        return false;
     }
 }
 
