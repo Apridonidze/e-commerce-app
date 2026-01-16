b41b49be8fc504ae1083400a05a536331f2b3f92
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-01-16 21:05:38 +0400

    feat: added ws.adminUser vairable in validatesocketadmin to have verified admins vairable in backend to work with easily

--- backend/socket.config/ValidateSocketAdmin.js
+++ backend/socket.config/ValidateSocketAdmin.js
@@ -4,12 +4,15 @@ async function ValidateSocketAdmin (user, ws) {
     try{
 
         const [isAdmin] = await db.query('select * from admin where id = ?',[user.userId])  
-        if(isAdmin.length === 0) {ws.send(JSON.stringify({type : "admin_access" , status : 403 , admin_access : false})) ; return false};
+        if(isAdmin.length === 0) {ws.send(JSON.stringify({type : "admin_access" , status : 403 , admin_access : false})) ; ws.adminUser = null;return false};
         
+        ws.adminUser = user
+
         ws.send(JSON.stringify({type : "admin_access" , status : 200 , admin_access : true}))
         return true;
 
     }catch(err){
+        ws.adminUser = null
         ws.send(JSON.stringify({type : "admin_access" , status : 500 , admin_access : false}))
         ws.send(JSON.stringify({type : "internal_error" , message : err}))
         ws.close()
