bee56b8fa63bc19ba5e86683f5a832dc86efce35
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-01-16 16:36:39 +0400

    feat: created AdminSupportChatSidebar.jsx component where clients messages , chats and retrun button will be displayed

---
+++ frontend/src/component/AdminSupportChatSidebar.jsx
@@ -0,0 +1,11 @@
+const AdminSupportChatSidebar = () => {
+    return(
+        <div className="admin-support-chat-sidebar">
+            {/* add return button that redirects admin to admin dashbaord */}
+            {/* add main chat here */}
+            admin sidebar
+        </div>
+    )
+}
+
+export default AdminSupportChatSidebar;
