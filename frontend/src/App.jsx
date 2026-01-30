0abc768fc01744dd21dc94f8bfdb4b908bc071e1
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-01-30 19:24:04 +0400

    comments : added comments in App.jsx about new routes and security of them

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,16 +15,20 @@ const App = () => {
   const router = createBrowserRouter([
     {path : '/' , element : <Main />},
     {path : '/landing-page' , element : <Landing />},
-    {path : '/reports' , element : <></>},
     {path : '/dashboard' , element : <Dashboard />},
     {path : '/admin-dashboard' , element : <AdminDashboard/>},
+    //add /admin-dashboard/pending-products
+    //add /admin-dashboard/onway-products
+    //add /admin-dashboard/delivered-products
     {path : '/admin-dashboard/admin-support-chat' , element  :<AdminSupportChatContainer />},
     {path : '/login' , element : <Login />},
     {path : '/sign' , element : <Sign />},
-    {path : '*' , element : <Landing />}, /**add error page */
+    {path : '*' , element : <></>}, /**add 404 page for this and button to go back to main page */
+
     //add feedbacks send page
     //add report send page
     //add credit cart fillout form
+    //add  which pages should be used with cookies rest left empty
     //add page for each page with likes , orders , comments  and data
   ])
 
