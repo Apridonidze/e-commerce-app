ea4d0f25a665ae7b33dfec0fc8b60ea81952a91a
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-02-03 16:53:31 +0400

    feat: added routes for products based on their status to make it easier for admins to navigate/edit orders

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,6 +8,9 @@ import Sign from './container/Sign'
 import Dashboard from './container/Dashboard'
 import AdminDashboard from './container/AdminDashboard'
 import AdminSupportChatContainer from './container/AdminSupportChatContainer'
+import Pendings from './component/Pendings'
+import OnWayProducts from './component/OnWayProducts'
+import DeliveredProducts from './component/DeliveredProducts'
 
 
 const App = () => {
@@ -17,19 +20,17 @@ const App = () => {
     {path : '/landing-page' , element : <Landing />},
     {path : '/dashboard' , element : <Dashboard />},
     {path : '/admin-dashboard' , element : <AdminDashboard/>},
-    //add /admin-dashboard/pending-products
-    //add /admin-dashboard/onway-products
-    //add /admin-dashboard/delivered-products
+    {path : '/admin-dashboard/pending-products' , element : <Pendings/>},
+    {path : '/admin-dashboard/onway-products' , element : <OnWayProducts/>},
+    {path : '/admin-dashboard/delivered-products' , element : <DeliveredProducts/>},
     {path : '/admin-dashboard/admin-support-chat' , element  :<AdminSupportChatContainer />},
     {path : '/login' , element : <Login />},
     {path : '/sign' , element : <Sign />},
     {path : '*' , element : <></>}, /**add 404 page for this and button to go back to main page */
-    //add page for each product
+    //add page for each product with likes , orders , comments  and data
     //add feedbacks send page
-    //add report send page
     //add credit cart fillout form
     //add  which pages should be used with cookies rest left empty
-    //add page for each page with likes , orders , comments  and data
   ])
 
   //add localstorage theme get method, and style project based on its references (e.g : dark, light)
