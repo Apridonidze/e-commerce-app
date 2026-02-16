b0a22f21c01fe3e8392bbfc03abcf6ea541ed727
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-02-16 19:44:12 +0400

    feat: added controllers for auth folder

---
+++ backend/controllers/auth/index.js
@@ -0,0 +1,8 @@
+const sign = require('./sign')
+const login = require('./login')
+const logout = require('./logout')
+const me = require('./me')
+
+module.exports = {
+    sign, login, logout, me
+}
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const router = express.Router();
 
+const authRoute = require('../controllers/auth')
 
+router.use('/sign', authRoute)
 
 
 module.exports = router
