1561b5ec5a4fb3894dd9c1a6ee0366da317f026a
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-02-17 15:34:55 +0400

    feat: created routes for reports services

---
+++ backend/controllers/reports/clear.js
@@ -0,0 +1,5 @@
+async function clear(req,res) {
+    // clear report by id , and return user message that their report has beeen viewed and thanks thme
+}
+
+module.exports = clear
--- backend/controllers/reports/index.js
+++ backend/controllers/reports/index.js
@@ -1,5 +1,6 @@
 const addPlatformReport = require('./add.platform.report')
 const addProductReport = require('./add.prod.report')
 const list = require('./list')
+const clear = require('./clear')
 
-module.exports = {addPlatformReport , addProductReport,list}
+module.exports = {addPlatformReport , addProductReport,list, clear}
