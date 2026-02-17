791a44e8bdbfd278800d6b47829a8088da6d085a
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-02-17 15:51:22 +0400

    feat: added feedback services routes

---
+++ backend/routes/feedback.route.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+
+const feedbackRoutes = require('../controllers/feedback')
+const ValidateToken = require('../middlewares/ValidateToken')
+
+router.get('/feedback', feedbackRoutes.list)
+router.get('/product-feedback/:id', feedbackRoutes.getProdFb)
+router.post('/platform-feedback', ValidateToken, feedbackRoutes.add)
+router.post('/product-feedback/:id', ValidateToken, feedbackRoutes.addProdFb)
+
+module.exports = router
