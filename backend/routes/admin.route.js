448c5b7fc7225dd27bb95551c7a7a58015b48808
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-02-17 13:00:48 +0400

    feat: added routes for admin api services

--- backend/routes/admin.route.js
+++ backend/routes/admin.route.js
@@ -1,7 +1,15 @@
 const express = require('express')
 const router = express.Router();
 
+const adminRoute = require('../controllers/admin');
+
+const rateLimiter = require('../middlewares/RateLimiter')
+const ValidateToken = require('../middlewares/ValidateToken');
+const isAdmin = require('../middlewares/isAdmin')
 
 
+router.use('/admin-list' , ValidateToken, isAdmin , adminRoute.list)
+router.use('/add-new-admin' , ValidateToken, isAdmin ,rateLimiter, adminRoute.add)
+router.use('/remove-admin' , ValidateToken, isAdmin ,rateLimiter, adminRoute.remove)
 
 module.exports = router
