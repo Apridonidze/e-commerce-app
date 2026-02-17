b6dda1ee1b818080278f8a047f17cec658adcd03
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-02-17 17:19:26 +0400

    fix: removed nonexsistant routes from manage.orders.route.js

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -4,9 +4,9 @@ const router = express.Router();
 const authRoute = require('../controllers/auth');
 const ValidateToken = require('../middlewares/ValidateToken');
 
+router.get('/me' , ValidateToken , authRoute.me)
 router.post('/sign', authRoute.sign)
 router.post('/login' , authRoute.login)
-router.get('/me' , ValidateToken , authRoute.me)
 router.delete('/logout' , ValidateToken, authRoute.logout)
 
 module.exports = router
--- backend/routes/manage.orders.route.js
+++ backend/routes/manage.orders.route.js
@@ -7,13 +7,11 @@ const ValidateToken = require('../middlewares/ValidateToken')
 const isAdmin = require('../middlewares/isAdmin')
 
 router.get('/order', ValidateToken, isAdmin, manageOrdersRoute.list)
-router.post('/order', ValidateToken, manageOrdersRoute.add)
 router.get('/order-count', ValidateToken, isAdmin, manageOrdersRoute.count)
 router.get('/pending-order', ValidateToken, isAdmin, manageOrdersRoute.pending)
 router.get('/onway-order', ValidateToken, isAdmin, manageOrdersRoute.onway)
 router.get('/delivered-order', ValidateToken, isAdmin, manageOrdersRoute.delivered)
 router.delete('/order', ValidateToken, manageOrdersRoute.remove)
-router.delete('/remove-admin-order', ValidateToken, isAdmin, manageOrdersRoute.removeAdmin)
 router.put('/order', ValidateToken, isAdmin, manageOrdersRoute.update)
 
 module.exports = router
