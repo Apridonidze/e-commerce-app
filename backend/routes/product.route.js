7ed97f98c86457a1e476e56bda13fa4984927acd
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-02-17 13:25:17 +0400

    feat: added routes for product services

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,7 +1,17 @@
 const express = require('express')
 const router = express.Router();
 
+const productRoute = require('../controllers/product');
+const ValidateToken = require('../middlewares/ValidateToken');
+const isAdmin = require('../middlewares/isAdmin');
 
+router.use('/create-product' , ValidateToken, isAdmin , productRoute.add)
+router.use('/edit-product/:id' , ValidateToken, isAdmin , productRoute.edit)
+router.use('/remove-product/:id' , ValidateToken, isAdmin , productRoute.remove)
+router.use('/products', productRoute.list)
+router.use('/similar-products', productRoute.similarList)
+router.use('/product-details', productRoute.productDetails)
+router.use('/search-product', productRoute.ProductSearch)
 
 
 module.exports = router
