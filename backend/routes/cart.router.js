368a6b27cfb5c2365ddc6084bcba7aa92cab5c4d
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-02-17 13:09:33 +0400

    feat: added routes for cart services

--- backend/routes/cart.router.js
+++ backend/routes/cart.router.js
@@ -1,7 +1,14 @@
-const express = require('express')
+const express = require('express');
 const router = express.Router();
 
+const cartRoute = require('../controllers/cart')
 
+const ValidateToken = require('../middlewares/ValidateToken');
+const RateLimiter = require('../middlewares/RateLimiter');
+
+router.use('/my-cart' , ValidateToken , cartRoute.list)
+router.use('/add-to-cart/:id' , ValidateToken , RateLimiter, cartRoute.list)
+router.use('/remove-from-cart/:id' , ValidateToken , RateLimiter, cartRoute.list)
 
 
 module.exports = router
