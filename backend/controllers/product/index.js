ce10543bc75a961a6dfe0fc8d0456a7cf8b8d355
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-02-17 13:22:13 +0400

    feat: created index.js for product services

---
+++ backend/controllers/product/index.js
@@ -0,0 +1,10 @@
+const add = require('./add');
+const edit = require('./edit');
+const remove = require('./remove');
+const list = require('./list');
+const similarList = require('./similar.list')
+const productDetails = require('./product.details');
+const ProductSearch = require('./product.search');
+
+
+module.exports = {add, edit , remove, list, similarList, productDetails, ProductSearch}
