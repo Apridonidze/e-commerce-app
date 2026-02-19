a8ca1815c585fb703c7b2e80d258b8e77f68c54a
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-02-19 20:12:25 +0400

    feat: implemented UserContext into Users.jsx

--- frontend/src/component/User.jsx
+++ frontend/src/component/User.jsx
@@ -1,38 +1,14 @@
+import { useContext } from 'react'
+import { UserContext } from '../context/UserContext'
+
 import Skeleton from 'react-loading-skeleton'
 import 'react-loading-skeleton/dist/skeleton.css'
 
-import axios from 'axios'
-
-import { useState,useEffect } from 'react'
-import { useCookies } from 'react-cookie'
-
-import { BACKEND_URL } from '../../config'
 
 const User = () => {
 
-    const [cookies] = useCookies(['token'])
-    const [user,setUser] = useState(null)
-    const [isAdmin, setIsAdmin] = useState(null)
-
-    useEffect(() => {
-        
-        const fetchUser = async() => {
-            try{
-
-                await Promise.all([
-                    axios.get(`${BACKEND_URL}/api/auth/me` , {headers : {Authorization : `Bearer ${cookies.token}`}}).then(resp => {console.log(resp) , setUser(resp.data.user)}),
-                    axios.get(`${BACKEND_URL}/api/admin` , {headers : {Authorization : `Bearer ${cookies.token}`}}).then(resp => {console.log(resp) , setIsAdmin(resp.data.isAdmin)}),
-                ])
-
-            }catch(err){
-                console.log(err)
-            }
-        }
-
-
-        return () => {fetchUser()}
-
-    },[cookies.token])
+    const { user, cartIds } = useContext(UserContext)
+    
 
     return(
         <section id="user">
@@ -41,7 +17,7 @@ const User = () => {
                 <span className='position-relative bg-white' style={{bottom: '15px'}}>{'Account'|| <Skeleton />}</span>
 
                 <h1>{user?.id || <Skeleton />}</h1>
-                <h1>{user?.fullname || <Skeleton />} {isAdmin !== null && isAdmin ? `Admin` : <></> || <Skeleton />}</h1>
+                <h1>{user?.fullname || <Skeleton />} {user !== null && user.role === 'admin' ? `Admin` : <></> || <Skeleton />}</h1>
                 <h1>{user?.email || <Skeleton />}</h1>
                 <h1>{[user?.country_code , ' ' , user?.phone]|| <Skeleton />}</h1>
                     {/* add logout btn */}
