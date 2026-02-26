6df0099febda7fe7777ae7877a0d1d9cdcc849b5
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-02-26 19:14:10 +0400

    feat: added route for stirpe webhook

--- backend/routes/stripe.route.js
+++ backend/routes/stripe.route.js
@@ -6,6 +6,7 @@ const route = require('../controllers/stripe')
 const ValidateToken = require('../middlewares/ValidateToken')
 
 router.get('/my-customer-id' , ValidateToken,  route.me)
+router.post('/webhook', express.raw({ type: 'application/json' }) ,route.webhook)
 router.post('/create-setup-intent' , ValidateToken ,route.createSetup)
 router.post('/create-customer-intent' , ValidateToken , route.createCustomer)
 
