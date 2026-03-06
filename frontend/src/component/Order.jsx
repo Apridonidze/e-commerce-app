6834865169ce125252892c2111c744bee52bfd1a
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-03-06 17:50:35 +0400

    comments : added todo comments in Order.jsx

--- frontend/src/component/Order.jsx
+++ frontend/src/component/Order.jsx
@@ -1,4 +1,10 @@
+import { useCookies } from "react-cookie"
+import { BACKEND_URL } from "../../config"
+import axios from "axios"
+
 const Order = ({ cart }) => {
+
+    const [cookies] = useCookies(['token'])
 
     const orderItems = async() => {
         try{
@@ -11,11 +17,14 @@ const Order = ({ cart }) => {
             console.log(err)
         }
     }
-
+ 
     return(
         <div className="order-container">
             <h1>Choose Items to be delivered</h1>
-
+            {/* display cart items with checkboxes */}
+            {/* once the order price is higher than 40gel make order button avaliable else disable it */}
+            {/* for avalaible button trigger orderItems function */}
+            {/* add alerts messages  */}
         </div>
     )
 }
