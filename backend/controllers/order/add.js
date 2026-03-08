23e962c0b3185bce556d6e4f183fc9d1cd42abe6
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-03-08 21:43:26 +0400

    mod: modified orders add.js service to reduce amount of items that other users can buy after completing ordering

--- backend/controllers/order/add.js
+++ backend/controllers/order/add.js
@@ -8,9 +8,11 @@ async function add(req,res) {
         const totalPrice = req.body.totalPrice
 
         const [order] = await db.query('insert into orders (user_id, total_price, status, address) values (?, ?, ?, ?)', [req.user.userId, totalPrice, 'Pending' , address])
-        await Promise.all(items.map((item) => {
-            db.query('insert into ordered_items (order_id, product_id, amount, price) values (?, ?, ?, ?)' , [order.insertId, item.id, item.amount, item.price])
-        }))
+        await Promise.all(items.map((item) => Promise.all([
+            db.query('insert into ordered_items (order_id, product_id, amount, price) values (?, ?, ?, ?)' , [order.insertId, item.id, item.amount, item.price]),
+            db.query('update products set amount = amount - ? where products_id = ?' , [item.amount,item.id])
+            ])  
+        ))
         await db.query('delete from cart where id = ?' , req.user.userId)
 
         return res.status(200).json({message : "Your Items Has Been Ordered Successfully, Wait For Delivery"})
