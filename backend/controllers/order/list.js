fa023976dd4bf1c09285898437ff47b5e656e529
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-03-08 21:49:19 +0400

    feat: developed list.js service in orders route to show my orders to track statuses

--- backend/controllers/order/list.js
+++ backend/controllers/order/list.js
@@ -1,5 +1,13 @@
+const db = require('../../middlewares/db')
+
 async function list(req,res) {
-    
+    try{
+
+        const [orders] = await db.query("select orders.status, orders.created_at, ordered_items.* from orders join ordered_items on ordered_items.order_id = orders.order_id where user_id = ?" , [req.user.userId])
+        console.log(orders)
+    }catch(err){
+        return res.status(500).json({errMessage : "Internal Error" , err : err})
+    }
 }
 
 module.exports = list
