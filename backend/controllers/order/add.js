e046caad1206eb3f43c9e35b7a2df739d157f7c6
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-03-08 19:36:01 +0400

    feat: created table for ordered_items that will be joined with orders;

--- backend/controllers/order/add.js
+++ backend/controllers/order/add.js
@@ -15,7 +15,7 @@ async function add(req,res) {
         // const updateStatus = productIds.map(prod => db.query('update cart set status = ? set date = ? where product_id = ? and id = ?' , ['pending' , date ,prod , req.user.userId]))
         // const resp = updateStatus[0][0]
 
-        // return res.status(200).json({message : "Your Items Has Been Ordered Successfully, Wait For Delivery", products : resp})
+        // return res.status(200.json({message : "Your Items Has Been Ordered Successfully, Wait For Delivery", products : resp})
 
     }catch(err){
         console.log(err)
