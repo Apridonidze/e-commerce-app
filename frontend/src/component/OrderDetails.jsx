49de0ee6640d1b5cb99e8c699ae3f7a1d10ee058
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-03-08 22:06:06 +0400

    feat: implemented order layout in OrderDetails.jsx component

--- frontend/src/component/OrderDetails.jsx
+++ frontend/src/component/OrderDetails.jsx
@@ -1,7 +1,9 @@
 const OrderDetails = ({order, orderId, key}) => {
     return(
-        <div className="order-details-container">
-
+        <div className="order-details-container" key={orderId}>
+            <h1>Order Status : {order.status}</h1>
+            <h3>Ordered At: {new Date(order.created_at).toLocaleDateString()} {new Date(order.created_at).toLocaleTimeString()}</h3>
+            <h3>Expected Delivery: {new Date(new Date(order.created_at).getTime() + 7 * 24 * 60 * 60 * 1000).toLocaleDateString()}</h3>
         </div>
     )
 }
--- frontend/src/component/OrderList.jsx
+++ frontend/src/component/OrderList.jsx
@@ -2,6 +2,7 @@ import axios from "axios"
 import { useEffect, useState } from "react"
 import { BACKEND_URL } from "../../config"
 import { useCookies } from "react-cookie"
+import OrderDetails from "./OrderDetails"
 
 const OrderList = () => {
 
@@ -35,11 +36,7 @@ const OrderList = () => {
         <div className="order-list-container">
             My Orders
             {orders?.map((order,orderId) => (
-                <>
-                    <h1>Order Status : {order.status}</h1>
-                    <h3>Ordered At: {new Date(order.created_at).toLocaleDateString()} {new Date(order.created_at).toLocaleTimeString()}</h3>
-                    <h3>Expected Delivery: {new Date(new Date(order.created_at).getTime() + 7 * 24 * 60 * 60 * 1000).toLocaleDateString()}</h3>
-                </>
+                <OrderDetails order={order} orderId={orderId} key={orderId}/>
             ))}
         </div>
     )
