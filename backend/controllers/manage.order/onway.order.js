cce5f9796584065ecd0f95c851b981c6e0ef7636
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-03-09 11:50:04 +0400

    mod: modified pending.order.js and onway.order.js to return correcntly formatted orders

--- backend/controllers/manage.order/onway.order.js
+++ backend/controllers/manage.order/onway.order.js
@@ -3,13 +3,12 @@ const db = require('../../middlewares/db')
 async function onway(req,res) {
     try{
         
-        // const [ OnWayProducts ] = await db.query()
-        // if(OnWayProducts.length < 1) return res.status(400).json({message : "No Products Beign Ordered" , products : []})
+        const [ OnWayProducts ] = await db.query('select orders.*, users.fullname, users.email from orders join users on orders.user_id = users.id where orders.status = ?' , ['OnWay'])
+        if(OnWayProducts.length < 1) return res.status(204)
 
-        // return res.status(200).json({message : "Products On Way" , products: OnWayProducts})
+        return res.status(200).json({message : "Products On Way" , products: OnWayProducts})
 
     }catch(err){
-        console.log(err)
         return res.status(500).json({errMessage : "Internal Error" , err : err})
     }
 }
--- backend/controllers/manage.order/pending.order.js
+++ backend/controllers/manage.order/pending.order.js
@@ -3,14 +3,12 @@ const db = require('../../middlewares/db')
 async function pending(req,res) {
      try{
     
-        // const [ PendingProducts ] = await db.query()
-        // if(PendingProducts.length < 1) return res.status(400).json({message : "No Products Beign Ordered" , products : []})
+        const [ PendingProducts ] = await db.query('select orders.*, users.fullname, users.email from orders join users on orders.user_id = users.id where orders.status = ?' , ['Pending'])
+        if(PendingProducts.length < 1) return res.status(204)
 
-        // return res.status(200).json({message : "Ordered Products" , products : PendingProducts})
-
+        return res.status(200).json({message : "Ordered Products" , products : PendingProducts})
 
     }catch(err){
-        console.log(err)
         return res.status(500).json({errMessage : 'Internal Error' , err : err})
     }
 }
