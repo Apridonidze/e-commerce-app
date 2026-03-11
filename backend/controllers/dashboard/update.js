c21c513e1d2e680cac1967164bd20b212706b0e5
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-03-11 12:08:16 +0400

    feat: developed service to update order status + added comments to do

--- backend/controllers/dashboard/update.js
+++ backend/controllers/dashboard/update.js
@@ -1,8 +1,19 @@
+const db = require('../../middlewares/db')
+
 async function update(req,res) {
     try{
 
+        const orderId = req.params.orderId
+        const status = req.body.status
+
+        await db.query('update orders set status = ? where order_id = ?' , [status, orderId])
+        return res.status(200).json({message : "Order Updated Successfully"})
+
+        // add node mailer for user whos order status got updated
+
     }catch(err){
         console.log(err)
+        return res.status(500).json({message : "Internal Error" , err})
     }
 }
 
