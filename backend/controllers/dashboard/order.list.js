5162ff63f5fa0fc3dc358c5f6ddcfcd23616abf4
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-03-12 21:06:11 +0400

    feat: created order.list.js service to get all orders for OrdersPage.jsx compoentnt with offsets

--- backend/controllers/dashboard/order.list.js
+++ backend/controllers/dashboard/order.list.js
@@ -1,6 +1,6 @@
 async function  orderList (req,res) {
     try{
-        
+        let
     }catch(err){
         console.log(err)
         return res.status(500).json({message : "Internal Error" , err})
