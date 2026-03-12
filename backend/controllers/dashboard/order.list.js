4b0ab09db1bcfe07536003dead6396e66c798f66
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-03-12 21:52:12 +0400

    feat: developed order.list.js api service to return orders based on offset (for optimization) and status (to display orders by statuses on OrdersPage.jsx)

--- backend/controllers/dashboard/order.list.js
+++ backend/controllers/dashboard/order.list.js
@@ -2,13 +2,13 @@ const db = require("../../middlewares/db");
 
 async function orderList (req,res) {
     try{
-        const { status, offset } = req.params;
-        
-        // const limit = 15;
+        const { status, offset } = req.params; //filter statuses
+        const limit = 15;
 
-        // const [ orders ] = await db.query(`select orders.*, users.fullname, users.email from orders join users on orders.user_id = users.id where orders.order_status = ? limit ?` , [orderStatus, limit+ offset]);
+        const [ orders ] = await db.query(`select orders.*, users.fullname, users.email from orders join users on orders.user_id = users.id where orders.status = ? limit ?` , [status, limit+ Number(offset)]);
+        if(orders.length === 0) return res.status(204)
 
-        console.log(req.params)
+        return res.status(200).json({message : "Orders Fetched Successfully" , orders})
 
     }catch(err){
         console.log(err)
