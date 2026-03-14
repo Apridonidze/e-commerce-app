59ced7749c2bd1734951f3c18f1ddef79690777c
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-03-14 16:53:29 +0400

    feat: created DeleteReport.jsx and RespondReport.jsx component to make popups if admin decides to remove/respond to report from users;

---
+++ frontend/src/component/DeleteReport.jsx
@@ -0,0 +1,9 @@
+const DeleteReport = () => {
+    return(
+        <div className="delete-report-container">
+
+        </div>
+    )
+}
+
+export default DeleteReport
---
+++ frontend/src/component/RespondReport.jsx
@@ -0,0 +1,9 @@
+const RespondReport = () => {
+    return(
+        <div className="respond-report-container">
+
+        </div>
+    )
+}
+
+export default RespondReport;
