a587888a35cb0efa79710a8c4998eb1861f79526
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-03-14 16:51:29 +0400

    feat: created layout for Report.jsx

--- frontend/src/component/Report.jsx
+++ frontend/src/component/Report.jsx
@@ -2,10 +2,16 @@ import { Link } from "react-router-dom";
 
 const Report = ( { report, reportId ,key } ) => {
     return(
-        <div className="report-container" key={reportId}>
-            <span>{report.fullname} {report.email} {report.content}</span>
-            <br />  
-            <span>{report?.type == 'Product' ? <div>Reported Product : <Link to={`/product/${report.product_id}`}>{report.title}</Link></div> : <></>}</span>
+        <div className="report-container d-flex justify-content-between" key={reportId}>
+            <div className="report-start">
+                <span>{report.fullname} {report.email} {report.content}</span>
+                <br />  
+                <span>{report?.type == 'Product' ? <div>Reported Product : <Link to={`/product/${report.product_id}`}>{report.title}</Link></div> : <></>}</span>
+            </div>
+            <div className="report-end d-flex flex-column">
+                <button>Delete</button>
+                <button>Respond</button>
+            </div>
         </div>
     );
 };
