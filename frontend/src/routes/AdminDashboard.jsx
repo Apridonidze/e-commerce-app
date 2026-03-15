260e1ad5a2b681ef3f5f96b98b0231e18a4913c8
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-03-15 20:23:07 +0400

    feat: developed component for feedbacks for admin  dashboard and created route for it

--- frontend/src/routes/AdminDashboard.jsx
+++ frontend/src/routes/AdminDashboard.jsx
@@ -151,6 +151,7 @@ const AdminDashboard = () => {
 
                         <section id="feedbacks">
                             <h1>Feedbacks</h1>
+                            <Link to={'/admin-dashboard/feedbacks'}></Link>
                             {feedbacks?.length == 0 ? feedbacks?.map((feedback, feedbackId) => (
                                 <AdminFeedback feedback={feedback} feedbackId={feedbackId} key={feedbackId}/>
                             )) : "No Feedbacks"}
