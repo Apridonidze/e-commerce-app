5dac5486d3487d138fce783bb025c9ab65c4caa1
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-03-17 15:07:06 +0400

    fix: fixed orders discard api to delete orders instantly without error

--- backend/controllers/order/remove.js
+++ backend/controllers/order/remove.js
@@ -4,7 +4,6 @@ async function remove(req,res) {
     try{
 
         const { id } = req.params;
-        // validate id to be valid
 
         const [ response ] = await db.query('delete from orders where user_id = ? and order_id = ?' , [req.user.userId, Number(id)])
         if(response.affectedRows === 0) return res.status(400).json({message : "Order Not Found"})
