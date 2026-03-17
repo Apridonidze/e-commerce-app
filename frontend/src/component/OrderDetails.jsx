87b2b1897d4907423bbc7dc8606da9c54343f4eb
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-03-17 13:33:29 +0400

    feat: created discard order button inside a dropdown for OrderDetails to make user able to discard their orders

--- frontend/src/component/OrderDetails.jsx
+++ frontend/src/component/OrderDetails.jsx
@@ -1,9 +1,22 @@
+import { useState } from "react";
+
 const OrderDetails = ({order, orderId, key}) => {
+
+    const [toggleDrop, setToggleDrop] = useState(false)
+
     return(
-        <div className="order-details-container" key={orderId}>
-            <h1>Order Status : {order.status}</h1>
-            <h3>Ordered At: {new Date(order.created_at).toLocaleDateString()} {new Date(order.created_at).toLocaleTimeString()}</h3>
-            <h3>Expected Delivery: {new Date(new Date(order.created_at).getTime() + 7 * 24 * 60 * 60 * 1000).toLocaleDateString()}</h3>
+        <div className="order-details-container d-flex justify-content-between" key={orderId}>
+            <div className="order-start">
+                <h1>Order Status : {order.status}</h1>
+                <h3>Ordered At: {new Date(order.created_at).toLocaleDateString()} {new Date(order.created_at).toLocaleTimeString()}</h3>
+                <h3>Expected Delivery: {new Date(new Date(order.created_at).getTime() + 7 * 24 * 60 * 60 * 1000).toLocaleDateString()}</h3>
+            </div>
+            <div className="order-end border h-100 d-flex flex-column align-items-end">
+                <button className="btn btn-primary" onClick={() => setToggleDrop(!toggleDrop)}>:</button>
+                <div className="toggle text-white" style={{ display : toggleDrop ? 'flex' : 'none' , flexDirection : 'column',position : 'relative', top : '10px'}}>
+                    <button className="btn btn-danger">Discard Order</button>
+                </div>
+            </div>
         </div>
     )
 }
--- frontend/src/component/ReportInput.jsx
+++ frontend/src/component/ReportInput.jsx
@@ -1,4 +1,10 @@
+import { useLocation, useParams } from "react-router-dom"
+
 const ReportInput = () => {
+
+    const type = useLocation();
+    console.log(type)
+
     return(
         <div className="report-input-container">
         report input
