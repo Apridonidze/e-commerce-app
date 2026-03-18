7d6123a7ca455c8954ef4de346603b7d35676208
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-03-18 16:18:37 +0400

    feat: developed Sales.jsx route

--- backend/controllers/product/sales.list.js
+++ backend/controllers/product/sales.list.js
@@ -1,7 +1,17 @@
+const db = require('../../middlewares/db')
+
 async function salesList (req, res) {
+
+    const limit = 15
+    const offset = parseInt(req.query.offset) || 0;
+    const category = req.query.category || null;
+
     try{
 
-        
+        const [ salesProducts ] = await db.query("select * from products where sales_price != null");
+        if(salesProducts.length === 0) return res.status(204).send()
+
+        return res.status(200).json({message : "Products On Sale Found" , products : salesProducts})
 
     }catch(err){
         return res.status(500).json({message : "Internal Error" , err})
--- frontend/src/routes/Sales.jsx
+++ frontend/src/routes/Sales.jsx
@@ -1,10 +1,83 @@
-import Sidebar from "../layout/Sidebar"
+import axios from "axios"
+import { useCookies } from "react-cookie"
 
-const Sales = () =>{
+import { useEffect, useState } from "react"
+
+import { BACKEND_URL } from "../../config"
+
+import Category from "../component/Category"
+import Header from "../layout/Header"
+import SupportChatContainer from "../component/SupportChatContainer"
+import Sidebar from "../layout/Sidebar"
+import Product from "../component/Product"
+import StatusMessage from "../alerts/StatusMessage"
+
+import Skeleton from "react-loading-skeleton" //relocate skeletons for folder
+
+import { useContext } from "react"
+import { ProductContext } from "../context/ProductContext"
+import EditProduct from "../admin/components/EditProduct"
+import RemoveProduct from "../admin/components/RemoveProduct"
+
+const Sales = () => {
+
+    const { prevProducts } = useContext(ProductContext)
+
+    const [ cookies ] = useCookies(['token'])
+
+    const [products, setProducts] = useState([])
+    const [offset, setOffset] = useState(0)
+    const [category, setCategory] = useState(null);
+
+    const [toggleEdit , setToggleEdit] = useState({status : false, product: null});
+    const [toggleRemove , setToggleRemove] = useState({status : false, productId: null});
+    const [toggleSidebar, setToggleSidebar]= useState(false);
+    
+    const fetchProducts = async(offset, category) => {
+
+        try{
+
+            const product = await axios.get(`${BACKEND_URL}/api/product/sales-products`, { params : {offset, category} })
+            
+            if(product.status === 204) setProducts([])
+            setProducts(product.data.products)
+
+        }catch(err){
+            setProducts(prevProducts)
+            console.log(err)
+            //toggle allert message and pass errors
+        }
+    }
+    
+    useEffect(() => {
+
+        setProducts(prevProducts)
+        fetchProducts(offset,category);
+
+        return () => {fetchProducts()}
+    },[category, offset])
+    
     return(
-        <div className="sales-container">
-            <Sidebar />
-            <div className="sales-main-container">
+        <div className="sales-container container-fluid row border" style={{height : '100vh'}}>
+            {/* <StatusMessage /> */}
+
+            {toggleEdit.status ? <EditProduct setToggleEdit={setToggleEdit} toggleEdit={toggleEdit}/> : <></> }
+            {toggleRemove.status ? <RemoveProduct setToggleRemove={setToggleRemove} toggleRemove={toggleRemove}/> : <></> }
+
+            <div className="sales-start col">
+                <Sidebar /> 
+            </div>
+            <div className="sales-end col " style={{minHeight : '100vh'}}>
+
+                <Header />
+                <Category setCategory={setCategory} category={category} setProducts={setProducts} fetchProducts={fetchProducts} offset={offset}/>
+
+                <div className="products row">
+                    {products?.length < 1 ? <h1>No Products In This Category.</h1> : products?.map((prod,prodId) => <Product prod={prod} prodId={prodId} key={prodId} setToggleEdit={setToggleEdit} setToggleRemove={setToggleRemove}/>) || <Skeleton />}
+                    {products?.length % 15 !== 0 || products?.length === 0 ? <></> : <button className="btn btn-warning" onClick={() => setOffset((prev) => {if(products.length % 15 === 0){return prev + 15} return})}>Load More...</button>}
+                </div>
+                
+                {!cookies ? <></> : <SupportChatContainer />}
                 
             </div>
         </div>
