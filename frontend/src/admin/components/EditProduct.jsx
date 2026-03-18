4d21af7ea01860fed8d85bab8207fcf2da7688c8
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-03-18 12:46:20 +0400

    feat: cloned EditProduct.jsx forms , functions , states and api calls from CreateProduct.jsx

--- frontend/src/admin/components/EditProduct.jsx
+++ frontend/src/admin/components/EditProduct.jsx
@@ -1,7 +1,283 @@
+import axios from "axios";
+import { useRef, useState } from "react"
+
+import { BACKEND_URL } from "../../../config";
+import { useCookies } from "react-cookie";
+
 const EditProduct = () => {
+
+     const categories = [
+        {
+            "name": "Select Product Category",
+            "slug": "",
+            "subcategories": []
+        },
+        {
+            "name": "Electronics",
+            "slug": "electronics",
+            "subcategories": [
+            "Select Product Sub-Category",
+            "Smartphones & Accessories",
+            "Laptops & Computers",
+            "PC Parts & Components",
+            "Gaming Consoles",
+            "Audio & Headphones",
+            "Smart Home",
+            "Cameras & Drones"
+            ]
+        },
+        {
+            "name": "Home & Living",
+            "slug": "home-living",
+            "subcategories": [
+            "Select Product Sub-Category",
+            "Furniture",
+            "Kitchen & Dining",
+            "Home Decor",
+            "Lighting",
+            "Cleaning Appliances"
+            ]
+        }, 
+        {
+            "name": "Fashion",
+            "slug": "fashion",
+            "subcategories": [
+            "Select Product Sub-Category",
+            "Men's Clothing",
+            "Women's Clothing",
+            "Shoes",
+            "Accessories",
+            "Watches & Jewelry"
+            ]
+        },
+        {
+            "name": "Beauty & Personal Care",
+            "slug": "beauty",
+            "subcategories": [
+            "Select Product Sub-Category",
+            "Skincare",
+            "Haircare",
+            "Makeup",
+            "Grooming Tools"
+            ]
+        },
+        {
+            "name": "Sports & Outdoors",
+            "slug": "sports-outdoors",
+            "subcategories": [
+            "Select Product Sub-Category",
+            "Fitness Equipment",
+            "Outdoor Gear",
+            "Sportswear",
+            "Cycling Accessories"
+            ]
+        },
+        {
+            "name": "Automotive",
+            "slug": "automotive",
+            "subcategories": [
+            "Select Product Sub-Category",
+            "Car Accessories",
+            "Auto Parts",
+            "Motorcycle Gear"
+            ]
+        },
+        {
+            "name": "Kids & Toys",
+            "slug": "kids-toys",
+            "subcategories": [
+            "Select Product Sub-Category",
+            "Toys",
+            "Baby Essentials",
+            "Kids Clothing"
+            ]
+        }
+]
+
+    const [ cookies ] = useCookies(['token'])
+
+    const regexContainsSpecial = /[^\w\s]/;
+    const NumberRegex = /\d/;
+
+    const [images,setImages] = useState([])
+    const [name , setName] = useState('')
+    const [description , setDescription] = useState('')
+    const [selectedCat , setSelectedCat] = useState('')
+    const [selectedSub, setSelectedSub] = useState('')
+    const [price ,setPrice] = useState('')
+    const [toggleSalesPrice, setToggleSalesPrice] = useState(false);
+    const [salesPrice,setSalesPrice] = useState('');
+    const [amount, setAmount] = useState('')
+
+    const [imageErr , setImageErr] = useState('')
+    const [nameErr, setNameErr] = useState('')
+    const [descErr, setDescErr] = useState('')
+    const [categoryErr, setCategoryErr] = useState('')
+    const [subCategoryErr, setSubCategoryErr] = useState('')
+    const [priceErr ,setPriceErr] = useState('')
+    const [salesPriceErr, setSalesPriceErr] = useState('')
+    const [amountErr ,setAmountErr] = useState('')
+
+    const imageRef = useRef(null)
+    const nameRef = useRef(null)
+    const descRef = useRef(null)
+    const categoryRef = useRef(null)
+    const subCategoryRef = useRef(null)
+    const priceRef = useRef(null)
+    const salesPriceRef = useRef(null)
+    const amountRef = useRef(null)
+    
+    const handleUploadProduct = async (e) => {
+        
+        e.preventDefault();
+
+        let isValid
+        let data
+        
+        const now = new Date()
+        const date = now.toLocaleDateString('en-GB')
+
+        if(images.length < 1){isValid = false ; setImageErr(`This Field Can't Be Empty`); imageRef.current.classList.add('is-invalid');imageRef.current.classList.remove('is-valid')}
+        else {isValid = true; setImageErr('') ;imageRef.current.classList.add('is-valid') ;imageRef.current.classList.remove('is-invalid')}
+
+        if(name.trim() == '' || name.trim() == null || name.trim() == undefined){isValid = false ; setNameErr(`This Field Can't Be Empty`); nameRef.current.classList.add('is-invalid');nameRef.current.classList.remove('is-valid')}
+        else if(name.trim().length <= 3){isValid = false; setNameErr('Enter Valid Product Name'); nameRef.current.classList.add('is-invalid');nameRef.current.classList.remove('is-valid')}
+        else if(regexContainsSpecial.test(name) === true){isValid = false; setNameErr('Your Product Name Should Not Contain Special Characters'); nameRef.current.classList.add('is-invalid');nameRef.current.classList.remove('is-valid')}
+        else {isValid = true; setNameErr('') ;nameRef.current.classList.add('is-valid') ;nameRef.current.classList.remove('is-invalid'); data = {...data,name:name}}
+
+        if(description.trim() == '' || description.trim() == null || description.trim() == undefined){isValid = false ; setDescErr(`This Field Can't Be Empty`); descRef.current.classList.add('is-invalid');descRef.current.classList.remove('is-valid')}
+        else if(description.trim().length <= 3){isValid = false; setDescErr('Enter Valid Product Description'); descRef.current.classList.add('is-invalid');descRef.current.classList.remove('is-valid')}
+        else {isValid = true; setDescErr('') ;descRef.current.classList.add('is-valid') ;descRef.current.classList.remove('is-invalid'); data = {...data, description : description}}
+
+        if(price.trim() == '' || price.trim() == null || price.trim() == undefined){isValid = false ; setPriceErr(`This Field Can't Be Empty`); priceRef.current.classList.add('is-invalid');priceRef.current.classList.remove('is-valid')}
+        else if(Number(price) === 0 || Number(price) >= 100000 || Number(price) <= 0){isValid = false; setPriceErr('Enter Valid Price'); priceRef.current.classList.add('is-invalid');priceRef.current.classList.remove('is-valid')}
+        else if (NumberRegex.test(price) === false){isValid = false ; setPriceErr('Enter Valid Price (Numbers Only)')}
+        else if (Number(salesPrice) > Number(price) || Number(salesPrice) === Number(price)){isValid = false ; setPriceErr('Sales Price Should Not Be Greater Than Original Price'); priceRef.current.classList.add('is-invalid');priceRef.current.classList.remove('is-valid')}
+        else {isValid = true; setPriceErr('') ;priceRef.current.classList.add('is-valid') ;priceRef.current.classList.remove('is-invalid'); data = {...data, price : Number(price)}}
+
+        if(toggleSalesPrice){
+            if(salesPrice.trim() == '' || salesPrice.trim() == null || salesPrice.trim() == undefined){isValid = false ; setSalesPriceErr(`This Field Can't Be Empty`); salesPriceRef.current.classList.add('is-invalid');salesPriceRef.current.classList.remove('is-valid')}
+            else if(Number(salesPrice) === 0 || Number(price) >= 100000 || Number(price) <= 0){isValid = false; setSalesPriceErr('Enter Valid Price'); salesPriceRef.current.classList.add('is-invalid');salesPriceRef.current.classList.remove('is-valid')}
+            else if (NumberRegex.test(salesPrice) === false){isValid = false ; setSalesPriceErr('Enter Valid Price (Numbers Only)'); salesPriceRef.current.classList.add('is-invalid');salesPriceRef.current.classList.remove('is-valid')}
+            else if (Number(salesPrice) > Number(price) || Number(salesPrice) === Number(price)){isValid = false ; setSalesPriceErr('Sales Price Should Not Be Greater Than Original Price'); salesPriceRef.current.classList.add('is-invalid');salesPriceRef.current.classList.remove('is-valid')}
+            else {isValid = true; setSalesPriceErr('') ;salesPriceRef.current.classList.add('is-valid') ;salesPriceRef.current.classList.remove('is-invalid'); data = {...data, salesPrice : Number(salesPrice)}}
+        }
+
+        if(!toggleSalesPrice){
+            if (salesPriceRef.current) {
+                salesPriceRef.current.classList.remove('is-valid')
+                salesPriceRef.current.classList.remove('is-invalid')
+            }
+            setSalesPriceErr('')
+            data = { ...data, salesPrice: null }
+        }
+        
+        if(selectedCat.trim() == '' || selectedCat.trim() == null || selectedCat.trim() == undefined){isValid = false ; setCategoryErr(`This Field Can't Be Empty`); categoryRef.current.classList.add('is-invalid');categoryRef.current.classList.remove('is-valid')}
+        else {isValid = true; setCategoryErr('') ;categoryRef.current.classList.add('is-valid') ;categoryRef.current.classList.remove('is-invalid'); data = {...data, category : selectedCat}}
+
+        if(selectedSub.trim() == '' || selectedSub.trim() == null || selectedSub.trim() == undefined){isValid = false ; setSubCategoryErr(`This Field Can't Be Empty`); subCategoryRef.current.classList.add('is-invalid');subCategoryRef.current.classList.remove('is-valid')}
+        else if(selectedSub.trim().length <= 3){isValid = false; setSubCategoryErr('Enter Valid Product Description'); subCategoryRef.current.classList.add('is-invalid');subCategoryRef.current.classList.remove('is-valid')}
+        else {isValid = true; setSubCategoryErr('') ;subCategoryRef.current.classList.add('is-valid') ;subCategoryRef.current.classList.remove('is-invalid'); data = {...data, subCategory : selectedSub}}
+
+        if(amount.trim() == '' || amount.trim() == null || amount.trim() == undefined){isValid = false ; setAmountErr(`This Field Can't Be Empty`); amountRef.current.classList.add('is-invalid');amountRef.current.classList.remove('is-valid')}
+        else if(Number(amount) === 0 || Number(amount) >= 100000 || Number(amount) <= 0){isValid = false; setAmountErr('Enter Valid Amount'); amountRef.current.classList.add('is-invalid');amountRef.current.classList.remove('is-valid')}
+        else if (NumberRegex.test(Number(amount)) === false){isValid = false ; setAmountErr('Enter Valid Amount (Numbers Only)')}
+        else {isValid = true; setAmountErr('') ;amountRef.current.classList.add('is-valid') ;amountRef.current.classList.remove('is-invalid'); data = {...data, amount : Number(amount), date : date}}
+
+
+        if(isValid){
+
+            const formData = new FormData();
+
+            images.forEach(img => {
+                formData.append('images', img);
+            });
+
+            Object.entries(data).forEach(([key, value]) => {
+                formData.append(key, value);
+            });
+
+            try {
+                // mod api/product with api/product/${productId}
+                const response = await axios.post(`${BACKEND_URL}/api/product`, formData ,{headers: {Authorization: `Bearer ${cookies.token}`,"Content-Type": "multipart/form-data"}});
+                console.log(response.data);
+
+            } catch (err) {
+                console.log(err);
+            }
+        }
+    }
+
     return(
-        <div className="edit-product-container">
-            
+        <div className="create-product-container position-fixed bg-white" style={{left : '40vw' }} tabIndex={9999}>
+            <div className="create-products-top">
+                <h4>Edit Product</h4>
+            </div>
+            <div className="create-products-main">
+                <form onSubmit={handleUploadProduct} enctype="multipart/form-data">
+
+                    <div className="images-container">
+                        {images.map((img, imgId) => {return <img src={URL.createObjectURL(img)} style={{maxWidth: '200px' , height : 'auto', cursor : 'pointer'}} alt={img.name} key={imgId} onClick={(e) => {const newImages = images.filter((_, id) => id !== imgId) ; setImages(newImages)}}/>})}
+                    </div>
+
+                    <div className="form-floating">
+                        <input type="file" multiple  className="form-control" onChange={(e) => {const files = Array.from(e.target.files); setImages(prev => [...prev, ...files])}}  accept="image/*" ref={imageRef}/>
+                        <span>{imageErr}</span>
+                    </div>
+
+                    <div className="form-floating">
+                        <input className="form-control" type="text" id="title" placeholder="Product Name" ref={nameRef} onChange={(e) => setName(e.target.value)} value={name}/>
+                        <label htmlFor="title">Product Name</label>
+                        <span>{nameErr}</span>
+                    </div>
+
+                    <div className="form-floating">
+                        <input className="form-control" type="text" id="title" placeholder="Product Description" ref={descRef} onChange={(e) => setDescription(e.target.value)} value={description}/>
+                        <label htmlFor="title">Product Description</label>
+                        <span>{descErr}</span>
+                    </div>
+
+                    <div className="form-floating">
+                        <input className="form-control" id="priceId" placeholder="Product Price (In GEL)" ref={priceRef} onChange={(e) => setPrice(e.target.value)} value={price}/>
+                        <label htmlFor="priceId">Product Price (In GEL)</label>
+                        <span>{salesPriceErr}</span>
+                    </div>
+
+                    <div className="form-group">
+                        <input type="checkbox" id="salesCheckbox" onChange={() => setToggleSalesPrice(!toggleSalesPrice)}/>
+                        <label htmlFor="salesCheckbox">On Sale</label>
+                    </div>
+
+                    {toggleSalesPrice ? <div className="form-floating">
+                        <input className="form-control" id="salesPriceId" placeholder="Product Sales Price (In GEL)" ref={salesPriceRef} onChange={(e) => setSalesPrice(e.target.value)} value={salesPrice}/>
+                        <label htmlFor="salesPriceId">Sales Price (In GEL)</label>
+                        <span>{priceErr}</span>
+                    </div> : <></>}
+
+                    <select className="form-select" name="" id="" onChange={(e) => setSelectedCat(e.target.value)} value={selectedCat} ref={categoryRef}>
+                        {categories.map((cat, catId) => (
+                            <option value={cat.name} key={catId}>{cat.name}</option>
+                        ))}
+                    </select>
+                    <span>{categoryErr}</span>
+
+                    {selectedCat && 
+                        <select className="form-select" onChange={(e) => setSelectedSub(e.target.value)} value={selectedSub} ref={subCategoryRef}>
+                            {categories.filter(cat => cat.name === selectedCat)[0].subcategories.map((sub, subId) => <option key={subId} value={sub}>{sub}</option>)}
+                        </select>
+                    }
+                    <span>{subCategoryErr}</span>
+
+                    <div className="form-floating">
+                        <input className="form-control" id="amountId" placeholder="Stock / Amount In WareHouse" ref={amountRef} onChange={(e) => setAmount(e.target.value)} value={amount}/>
+                        <label htmlFor="priceId">Stock / Amount In WareHouse</label>
+                        <span>{amountErr}</span>
+                    </div>
+                    
+                    <input type="submit" value='Add Product'/>
+                    
+                </form>
+            </div>
         </div>
     )
 }
