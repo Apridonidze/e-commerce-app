9ae5bfb539eed21d49d3c05f6cc7fb55e7e7a207
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-03-18 13:00:05 +0400

    feat: created edit.js service to edit product details

---
+++ backend/controllers/product/edit.js
@@ -0,0 +1,41 @@
+const fs = require('fs')
+const db = require('../../middlewares/db')
+const NewProductSchema = require('../../schemas/NewProductSchema')
+
+async function edit(req,res) {
+
+    const data = req.body;    
+
+    const parsedRequest = {
+        name : data.name.toString(),
+        description : data.description.toString(),
+        price : Number(data.price),
+        salesPrice : Number(data.salesPrice),
+        category : data.category.toString(),
+        subCategory : data.subCategory.toString(),
+        amount :  Number(data.amount),
+        date : data.date
+    };
+
+    const validateProduct = NewProductSchema(parsedRequest);
+
+
+    if(!validateProduct.success) return res.status(400).json({errMessage : 'Invalid Input'})
+
+    try{
+        
+        const files = req.files;
+        const filesBuffer = await Promise.all(files.map(file => fs.promises.readFile(file.path)));
+        const base64 = filesBuffer.map(buffer => buffer.toString("base64"))
+
+        await db.query('insert into products (id, images, title, description , category , subcategory , price, sales_price, amount , date) values (?,?,?,?,?,?,?,?,?,?)' , [req.user.userId , [JSON.stringify(base64)] , parsedRequest.name , parsedRequest.description , parsedRequest.category , parsedRequest.subCategory, parsedRequest.price , parsedRequest.salesPrice, parsedRequest.amount , parsedRequest.date])
+        return res.status(200).json({message : 'product added succsefully' , productDetails : `${data.name}${data.description}${data.category}${data.subCategory}`})
+
+    }catch(err){
+        console.log(err)
+        return res.status(500).json({errMessage : 'Internal Error'  , err : err})
+    }
+   
+}
+
+module.exports = edit;
--- backend/controllers/product/index.js
+++ backend/controllers/product/index.js
@@ -4,5 +4,6 @@ const list = require('./list');
 const similarList = require('./similar.list')
 const productDetails = require('./product.details');
 const ProductSearch = require('./product.search');
+const edit = require('./edit');
 
-module.exports = {add , remove, list, similarList, productDetails, ProductSearch}
+module.exports = {add , remove, list, similarList, edit, productDetails, ProductSearch}
