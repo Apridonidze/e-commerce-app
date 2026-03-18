1cafaca109c83f7b4fef3966307c97e66ca0a64b
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-03-18 15:13:32 +0400

    feat :developed RemoveProduct.jsx component with api integration

--- frontend/src/admin/components/RemoveProduct.jsx
+++ frontend/src/admin/components/RemoveProduct.jsx
@@ -1,7 +1,31 @@
-const RemoveProduct = () => {
+import axios from "axios"
+import { BACKEND_URL } from "../../../config"
+import { useCookies } from "react-cookie"
+
+const RemoveProduct = ({ setToggleRemove, toggleRemove }) => {
+
+    const [ cookies ] = useCookies(['token'])
+
+    const handleDelete = async() => {
+        try{
+
+            const response = await axios.delete(`${BACKEND_URL}/api/product/${toggleRemove.productId}`, {headers : {Authorization : `Bearer ${cookies.token}`}})
+            console.log(response)
+            if(response.status === 400){
+                // toggle error message
+            }
+            // toggle success message
+            window.location.reload();
+        }catch(err){
+            console.log(err)
+        }
+    }
+
     return(
-        <div className="remove-product-container">
-            
+        <div className="remove-product-container position-absolute bg-danger" style={{zIndex : 999}}>
+            <h3>Delete Product</h3>
+            <button className="btn btn-danger" onClick={() => handleDelete()}>Delete</button>
+            <button className="btn btn-secondary" onClick={() => setToggleRemove({status : false, productId : null})}>Cancel</button>
         </div>
     )
 }
