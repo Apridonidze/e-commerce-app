e4e7e28f3f2f08137c61f796569cd233f0f384b7
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-03-20 13:21:59 +0400

    fix: fixed list.js service db query with inner join to fetch all products that is with sent status no matter if it has product id in it or not

--- backend/controllers/reports/list.js
+++ backend/controllers/reports/list.js
@@ -9,7 +9,7 @@ async function list(req,res) {
         
         //filter req.params to be number if not return 400 status code error
 
-        const [ ReportsList ] = await db.query('select users.fullname , users.email, reports.* , products.title , products.products_id from reports join users on users.id = reports.user_id left join products on reports.product_id = products.products_id where status = ? limit ?' , [ "Sent" , Number(offset) + limit])
+        const [ ReportsList ] = await db.query('select users.fullname , users.email, reports.* , products.title , products.products_id from reports join users on users.id = reports.user_id left join products on reports.product_id = products.products_id where status = ? limit ?' , ["Sent" , Number(offset) + limit])
         if(ReportsList.length === 0) return res.status(204)
 
         return res.status(200).json({message : "Reports Found" , reports : ReportsList})
