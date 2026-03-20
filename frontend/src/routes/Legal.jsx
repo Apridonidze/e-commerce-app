f5b275d7bcb850e692e604cf264a9f7604f7ee02
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-03-20 13:40:35 +0400

    feat: added smooth scrolling in Legal.jsx component based on location hash

--- frontend/src/routes/Legal.jsx
+++ frontend/src/routes/Legal.jsx
@@ -1,8 +1,17 @@
 import Sidebar from "../layout/Sidebar"
 import Privacy from "../component/Privacy"
 import Terms from "../component/Terms"
+import { useEffect } from "react"
+import { useLocation } from "react-router-dom"
 
 const Legal = () => {
+
+    const { hash } = useLocation()
+
+    useEffect(() => {
+        if (hash) {const el = document.querySelector(hash);if (el) {el.scrollIntoView({ behavior: "smooth" })}} ; return;
+    }, [hash]);
+
     return(
         <div className="legal-container d-flex">
             <Sidebar />
