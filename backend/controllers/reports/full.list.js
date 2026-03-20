fa8a06885368024210d2758cff685db3a1ac0d75
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-03-20 13:22:37 +0400

    fix : fixed full.list.js service db query with same inner join method

--- backend/controllers/reports/full.list.js
+++ backend/controllers/reports/full.list.js
@@ -11,7 +11,7 @@ async function fullList(req, res) {
         if (Number.isNaN(offset) || offset < 0) {return res.status(400).json({ message: "Invalid offset" })}
         if (status == "undefined") status = undefined
 
-        let query = `select users.fullname, users.email, reports.*, products.title from reports join users on users.id = reports.user_id join products on reports.product_id = products.products_id`
+        let query = `select users.fullname, users.email, reports.*, products.title from reports join users on users.id = reports.user_id left join products on reports.product_id = products.products_id`
         const params = []
 
         if (status) {
