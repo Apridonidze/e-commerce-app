fe557d8ed3fb656e8a6153199887e9105afcf0fa
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-03-22 02:54:15 +0400

    fix : fixed report add.js service query

--- backend/controllers/reports/add.js
+++ backend/controllers/reports/add.js
@@ -6,14 +6,12 @@ async function add(req,res) {
         const data = req.body
         //validate data in zod schema
 
-        console.log(data)
-        // add filter that if type === 'product' product id should be necceccary
-
-        const [ report ] = await db.query('insert into reports (id, type, content, product_id) values (?, ?, ?, ?, ?)' , [req.user.userId , data.type , data.content, data.productId ?? null , data.status])
+        const [ report ] = await db.query('insert into reports (user_id, type, content, product_id, status) values (?, ?, ?, ?, ?)' , [req.user.userId , data.type , data.content, data.productId ?? null , data.status])
         return res.status(200).json({message : "Report Send Successfully" , reportId : report.insertId })
 
 
     }catch(err){
+        console.log(err)
         return res.status(500).json({errMessage : "Internal Error" , err : err})
     }
 }
