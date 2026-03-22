194f7a62eb92964692f10b7d15637e181f889655
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-03-22 16:21:34 +0400

    feat: added api functiopns in AddToCart.jsx from Product.jsx

--- frontend/src/component/AddToCart.jsx
+++ frontend/src/component/AddToCart.jsx
@@ -1,7 +1,72 @@
+import { useState, useEffect } from "react";
+
+import { useContext } from "react";
+import { UserContext } from "../context/UserContext";
+
+import axios from "axios";
+import { BACKEND_URL } from "../../config";
+import { useCookies } from "react-cookie";
+
+
+
 const AddToCart = ({ setToggleAddToCart, toggleAddToCart }) => {
+
+    const [ cookies ] = useCookies(['token'])
+ 
+    const [isInCart, setIsInCart] = useState(false);
+    const [amount, setAmount] = useState(0);
+    const [inCartAmount, setInCartAmount] = useState(0);
+
+    const handleAddToCart = async (productId) => {
+        try {
+
+            await axios.post(`${BACKEND_URL}/api/cart/${productId}`,{ amount },{ headers: { Authorization: `Bearer ${cookies.token}` } })
+            setIsInCart(true)
+            setInCartAmount(amount)
+            setAmount(0)
+            // toggle success message
+            setToggleAddToCart({status : false, product : null})
+
+        } catch (err) {
+            // toggle alert message
+            setIsInCart(false)
+            setInCartAmount(0)
+            setAmount(amount)
+        }
+    }
+
+// refactor
+
     return(
-        <div className="add-to-cart">
-            Add To Cart
+        <div className="add-to-cart-container position-relative bg-white border top-50" style={{zIndex : 999}} key={toggleAddToCart.product.prodcuts_id} >
+            <div className="product-wrapper">
+        
+                {<img className="w-100 h-100 rounded-1" src={`data:image/svg+xml;base64,${JSON.parse(toggleAddToCart.product.images)[0]}`} style={{maxWidth:'200px'}}/> || <Skeleton />}
+                            
+                <div className="product-main">
+        
+                    <h5>{toggleAddToCart.product.title || <Skeleton count={1} width={'12vw'}/>}</h5>
+                    <h5>{`${toggleAddToCart.product.category} / ${toggleAddToCart.product.subcategory}` || <Skeleton count={2}/>}</h5>
+        
+                    <h5>{toggleAddToCart.product.price.toString().split('.').length > 1 ? toggleAddToCart.product.price : `${toggleAddToCart.product.price}.00` + '₾' || <Skeleton/>}</h5>
+                    <h5>{`Items Left: ${toggleAddToCart.product?.amount}`|| <Skeleton/>}</h5>
+        
+                </div>
+        
+                <div className="product-bottom d-flex align-items-center" >
+                            
+                    <div>
+                        <button disabled={isInCart ? true : false} onClick={() => setAmount(prev => { if(prev - 1 <= 0) return 0;return prev - 1})}>-</button>
+                        <span>{isInCart ? inCartAmount : amount}</span>
+                        <button disabled={isInCart ? true : false} onClick={() => setAmount(prev => { if(prev + 1 > toggleAddToCart.product.amount)return prev; return prev + 1})}>+</button>
+                    </div>
+
+                    <button onClick={() => handleAddToCart(toggleAddToCart.product.products_id)} disabled={isInCart || amount == 0 ? true : false}>Add To Cart</button>
+
+                </div>
+        
+            </div>
+                    
         </div>
     )
 }
