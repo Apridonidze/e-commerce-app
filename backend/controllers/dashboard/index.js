81c083a3cb93edfc288f0db2c9d8de9bbc8fd518
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-03-23 11:58:48 +0400

    refactor + cleanup + doc : organized dashboard controller services

--- backend/controllers/dashboard/index.js
+++ backend/controllers/dashboard/index.js
@@ -1,5 +1,5 @@
-const list = require("./list");
-const orderList = require("./order.list");
-const update = require('./update')
+const list = require("./list"); //service to fetch dashboard data
+const orderList = require("./order.list"); //service to fetch full order list with offset + status
+const update = require('./update'); //service to update order with orderId and status
 
-module.exports = {list, update , orderList}
+module.exports = {list, update , orderList}; //exporting services from controller
--- backend/controllers/dashboard/update.js
+++ backend/controllers/dashboard/update.js
@@ -1,33 +1,32 @@
-const z = require('zod');
+const z = require('zod'); //importing zod library
 
 const db = require('../../utils/db');
-const mailer = require('../../utils/mailer');
+const mailer = require('../../utils/mailer'); //importing utils
 
 async function update(req,res) {
 
     const Schema = z.object({
         orderId : z.number().min(0).max(999),
         status : z.enum(['Pending','OnWay','Delivered'])
-    });
+    }); //defining schema for request data
     
-    function validateParams (data) {return Schema.safeParse(data)};
+    function validateParams (data) {return Schema.safeParse(data)}; //validating data with Schema object
     
-    const validateParamsResponse = validateParams({orderId : Number(req.params.orderId), status : req.body.status});
-    if(!validateParamsResponse.success) return res.status(400).json({message: "Invalid Credidentials"});
-    
+    const validateParamsResponse = validateParams({orderId : Number(req.params.orderId), status : req.body.status}); //passsing data to validateParams
+    if(!validateParamsResponse.success) return res.status(400).json({message: "Invalid Credidentials"}); //returnign error message if validator fails
 
     try{
 
         const { orderId } = req.params;
-        const { status } = req.body;
+        const { status } = req.body; //defining request data
         
-        const [ data ] = await db.query('select orders.total_price, orders.address , users.email from orders join users on users.id = orders.user_id where order_id = ?' , [ orderId ])
-        if(data.length === 0) return res.status(404).json({message : "Order Not Found"})
+        const [ data ] = await db.query('select orders.total_price, orders.address , users.email from orders join users on users.id = orders.user_id where order_id = ?' , [ orderId ]); //selecting order and user data for updating order and sending mail
+        if(data.length === 0) return res.status(404).json({message : "Order Not Found"}); //returning 404 status code error if order not found
+ 
+        const row = data[0]; //defining users data as an object from array
 
-        const row = data[0]
-
-        const [ response ] = await db.query('update orders set status = ? where order_id = ?' , [status, orderId]);
-        if(response.affectedRows === 0) return res.status(404).json({message: "Order With Provided Id Does Not Exists."});
+        const [ response ] = await db.query('update orders set status = ? where order_id = ?' , [status, orderId]); //updating order with orderId and status
+        if(response.affectedRows === 0) return res.status(404).json({message: "Order With Provided Id Does Not Exists."}); //returning 404 status code if order is not modifed (if not modifed = does not exists)
 
         
         const html = `
@@ -44,13 +43,13 @@ async function update(req,res) {
                 
                 <p style="margin-top: 20px;">Thank you for your purchase.</p>
             </div>
-        `;
+        `; //html for mail 
 
         mailer({
             to: row.email,
             subject: 'Order Update',
             html,
-        }).catch(console.error); //sending mail to customer
+        }).catch(console.error); //sending mail to customer with error catching
         
         return res.status(200).json({message : "Order Updated Successfully", status}); //sending 200 stsatus code if order update success
 
