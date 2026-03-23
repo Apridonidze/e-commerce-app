4ebbe8b0e48d042e94994db9c7a211b5f6daf2ab
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-03-23 16:57:35 +0400

    feat: implemented zod validator in clear.js

--- backend/controllers/reports/clear.js
+++ backend/controllers/reports/clear.js
@@ -2,10 +2,23 @@ const db = require("../../utils/db")
 
 async function clear(req,res) {
     
-    const reportId = Number(req.params.id)
+    const reportId = req.params.id
     const resolution_action = req.body.selectReason
     const status = req.body.status
     const resolved_by = req.user.userId;
+
+    
+    const Schema = z.object({
+        resolution_action : z.enum(['Content Removed (Valid)','Content Removed (Invalid)','Listing Edited (Valid)','Listing Edited (Invalid)','Already Fixed (Valid)','Already Fixed (Invalid)','No Violation (Valid)','No Violation (Invalid)','Duplicate (Valid)','Duplicate (Invalid)','Other (Valid)','Other (Invalid)']),
+        reportId : z.preprocess(val => val !== undefined ? Number(val) : undefined,z.number().optional()),
+        status : z.enum(['Sent','Responded','Removed'])
+    });//defining schema for requests data
+                
+    function validateParams (data) {return Schema.safeParse(data)}; //definign functuon to valdiate data with schema provided
+                
+    const validateParamsResponse = validateParams({reportId , resolution_action, status}); //passing data to validator funciton 
+    if(!validateParamsResponse.success) return res.status(400).json({message: "Invalid Credidentials"});  //returning error message if validation fails    
+        
 
     try{
         // add node mailer to user whos report has been deleted
