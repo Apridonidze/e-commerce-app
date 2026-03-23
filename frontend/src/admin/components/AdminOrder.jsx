88b2e86da2946d19f00896f4ab2fe0f63731278d
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-03-23 11:44:10 +0400

    feat: added change status btton in AdmonOrder.jsx

--- frontend/src/admin/components/AdminOrder.jsx
+++ frontend/src/admin/components/AdminOrder.jsx
@@ -66,13 +66,14 @@ const AdminOrder = ({ order, orderId, key, setOrders }) => {
         <div className="order-details-container" key={orderId}>
             <div className="order-top  d-flex justify-content-between">
                 <div className="order-start">
-                    <h1>Order Status : {order.status}</h1>
+                    <h1>Order Status : {status}</h1>
                     <h3>Ordered At: {new Date(order.created_at).toLocaleDateString()} {new Date(order.created_at).toLocaleTimeString()}</h3>
                     <h3>Expected Delivery: {new Date(new Date(order.created_at).getTime() + 7 * 24 * 60 * 60 * 1000).toLocaleDateString()}</h3>
                 </div>
                 <div className="order-end border h-100 d-flex flex-column align-items-end">
                     <button className="btn btn-primary" onClick={() => setToggleDrop(!toggleDrop)}>:</button>
                     <div className="toggle text-white" style={{ display : toggleDrop ? 'flex' : 'none' , flexDirection : 'column',position : 'relative', top : '10px'}}>
+                        <button className="btn btn-primary" onClick={() => setStatus(() => {if(order.status === 'Pending') return 'OnWay'; if(order.status === 'OnWay') return 'Delivered' })} disabled={status === 'Delivered' ? true : false}>Change Status to {order.status === 'Pending' ? 'OnWay' : order.status === 'OnWay' ? 'Delivered' : 'Delivered'}</button>
                         <button className="btn btn-danger" onClick={() => discardOrder(order.order_id)}>Discard Order</button>
                     </div>
                 </div>
