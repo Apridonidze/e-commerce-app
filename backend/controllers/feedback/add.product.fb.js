9f1df5426a66a3a459ddfae03312f23636e63f51
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-03-23 12:31:07 +0400

    feat: added zod validation in feedback add.product.fb.js service

--- backend/controllers/feedback/add.product.fb.js
+++ backend/controllers/feedback/add.product.fb.js
@@ -1,16 +1,29 @@
+const z = require('zod')
 const db = require('../../utils/db')
 
 async function addByProdId(req,res) {
+
+    const data = req.body
+    const prodId = req.params.id
+    
+    const Schema = z.object({
+        content : z.string().min(0).max(255),
+        star : z.number().min(0).max(5),
+        prodId : z.number().min(0)
+    }); 
+            
+    function validateParams (data) {return Schema.safeParse(data)};
+            
+    const validateParamsResponse = validateParams({content : data.content, star : Number(data.star), prodId: Number(prodId)}); 
+    if(!validateParamsResponse.success) return res.status(400).json({message: "Invalid Credidentials"}); 
+
     try{
-        const data = req.body
-        const prodId = req.params.id
         
-        const [ newFeedback ] = await db.query('insert into feedback (id, content, stars, product_id, type) values (?, ?, ?, ?, ?)' , [req.user.userId , data.content, data.star, prodId, 'product'])
-        return res.status(200).json({message : "Feedback Sent Succesfully" , feedbackId : newFeedback.insertId})
+        await db.query('insert into feedback (id, content, stars, product_id, type) values (?, ?, ?, ?, ?)' , [req.user.userId , data.content, data.star, prodId, 'product'])
+        return res.status(200).json({message : "Feedback Sent Succesfully"})
 
     }catch(err){
-        console.log(err)
-        return res.status(500).json({errMessage : "Internal Error" , err : err})
+        return res.status(500).json({errMessage : "Could Not Send Feedback. Try Later"})
     }
 }
 
