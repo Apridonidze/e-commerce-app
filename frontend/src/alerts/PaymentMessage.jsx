8ee1ddf3536f242755bf9eac8c25edf55ef9443f
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-03-25 13:02:45 +0400

    feat: created PaymentMessage.jsx to alert users that order they placed has been paid successfully

---
+++ frontend/src/alerts/PaymentMessage.jsx
@@ -0,0 +1,9 @@
+const PaymentMessage = () => {
+    return(
+        <div className="payment-message-container">
+            Payment Message
+        </div>
+    )
+}
+
+export default PaymentMessage
