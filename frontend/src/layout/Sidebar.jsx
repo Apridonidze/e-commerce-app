e7e881f1f4a2e087c5b5b8b3e5f4bd33372b0251
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-04-01 19:55:57 +0400

    mod : modified sidebar urls with NavLinks to track which route is active

--- frontend/src/layout/Sidebar.jsx
+++ frontend/src/layout/Sidebar.jsx
@@ -1,5 +1,5 @@
 import { useCookies } from 'react-cookie'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import { useEffect , useState, useRef, useContext } from 'react'
 import { BACKEND_URL } from '../../config'
 import { UserContext } from '../context/UserContext'
@@ -60,48 +60,48 @@ const Sidebar = () => {
                 <div className="sidebar-center row g-4">
                     <div className="center-header col-12 h-auto">
                         <div className="text d-flex flex-column">
-                            <Link to='/'>Home</Link>
-                            <Link to='/sales'>On Sale</Link>
+                            <NavLink to='/' className={({ isActive }) => isActive ? "active-link" : ""}>Home</NavLink>
+                            <NavLink to='/sales' className={({ isActive }) => isActive ? "active-link" : ""}>On Sale</NavLink>
                         </div>
                     </div>
                     <div className="center-center col-12 h-auto">
                         {cookies?.token ? <div className="text d-flex flex-column">
-                            <Link to='/dashboard'>Dashboard</Link>
-                            <Link to='/dashboard#cart-items'>Cart</Link>
-                            <Link to='/dashboard#order-list'>Orders</Link>
+                            <NavLink to='/dashboard' className={({ isActive }) => isActive ? "active-link" : ""}>Dashboard</NavLink>
+                            <NavLink to='/dashboard#cart-items' className={({ isActive }) => isActive ? "active-link" : ""}>Cart</NavLink>
+                            <NavLink to='/dashboard#order-list' className={({ isActive }) => isActive ? "active-link" : ""}>Orders</NavLink>
                         </div> : <></>}
                     </div>
                     <div className="center-bottom col-12 h-auto">
                         {user !== null && user.role === 'admin' ? 
                             <div className="text d-flex flex-column gap-3">
                                 <div className="text-top d-flex flex-column">
-                                    <Link to='/admin-dashboard'>Admin Dashboard</Link>
-                                    <Link to='/admin-dashboard#manage-products'>Manage Products</Link>
-                                    <Link to='/admin-dashboard#reports'>Reports</Link>
-                                    <Link to='/admin-dashboard#feedbacks'>Feedbacks</Link>
+                                    <NavLink to='/admin-dashboard' className={({ isActive }) => isActive ? "active-link" : ""}>Admin Dashboard</NavLink>
+                                    <NavLink to='/admin-dashboard#manage-products' className={({ isActive }) => isActive ? "active-link" : ""}>Manage Products</NavLink>
+                                    <NavLink to='/admin-dashboard#reports' className={({ isActive }) => isActive ? "active-link" : ""}>Reports</NavLink>
+                                    <NavLink to='/admin-dashboard#feedbacks' className={({ isActive }) => isActive ? "active-link" : ""}>Feedbacks</NavLink>
                                 </div>
                                 <div className="text-bottom">
-                                    <Link to='/admin-dashboard/admin-support-chat'>Support Chat <span>{messagesCount < 9  ? messagesCount : '9+' }</span></Link> {/* add limit for messageCount (max 9 , after 9 return 9+) */}
+                                    <NavLink to='/admin-dashboard/admin-support-chat' className={({ isActive }) => isActive ? "active-link" : ""}>Support Chat <span>{messagesCount < 9  ? messagesCount : '9+' }</span></NavLink> {/* add limit for messageCount (max 9 , after 9 return 9+) */}
                                 </div>
                             </div> : <></>
                         }
                     </div>
                     <div className="center-auth col-12 h-auto">
                         {!cookies.token ? <div className="text d-flex flex-column">
-                            <Link to='/login'>Login</Link>
-                            <Link to='/sign'>Sign</Link>
+                            <NavLink to='/login' className={({ isActive }) => isActive ? "active-link" : ""}>Login</NavLink>
+                            <NavLink to='/sign' className={({ isActive }) => isActive ? "active-link" : ""}>Sign</NavLink>
                         </div> : <></>}
                     </div>
                 </div>
                 <div className="sidebar-end"> 
                     <div className="end-start">
-                        <Link to={'/faq'}>FAQ</Link>
-                        <Link to={`/leave-feedback`}>Feedback</Link>
-                        <Link to={'/report-platform'}>Report</Link>
+                        <NavLink to={'/faq'} className={({ isActive }) => isActive ? "active-link" : ""}>FAQ</NavLink>
+                        <NavLink to={`/leave-feedback`} className={({ isActive }) => isActive ? "active-link" : ""}>Feedback</NavLink>
+                        <NavLink to={'/report-platform'} className={({ isActive }) => isActive ? "active-link" : ""}>Report</NavLink>
                     </div>
                     <div className="end-main">
-                        <Link to={'/legal#terms'}>Terms of Service</Link>
-                        <Link to={'/legal#privacy'}>Privacy Policy</Link>
+                        <NavLink to={'/legal#terms'} className={({ isActive }) => isActive ? "active-link" : ""}>Terms of Service</NavLink>
+                        <NavLink to={'/legal#privacy'} className={({ isActive }) => isActive ? "active-link" : ""}>Privacy Policy</NavLink>
                         <span>Copyright 2026</span>
                     </div>
                 </div>
