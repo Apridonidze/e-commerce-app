74d322a8daa7addc643fcb353a3d1d50f915f64d
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-04-02 16:24:14 +0400

    cleanup + doc : cleaned up and documentated AddToCart.jsx component

--- frontend/src/component/AddToCart.jsx
+++ frontend/src/component/AddToCart.jsx
@@ -15,42 +15,52 @@ const AddToCart = ({ setToggleAddToCart, toggleAddToCart, setToggleAlert }) => {
     const handleAddToCart = async (productId) => {
         try {
 
-            const response = await axios.post(`${BACKEND_URL}/api/cart/${productId}` ,{ amount }, {headers: { Authorization: `Bearer ${cookies.token}`}})
+            const response = await axios.post(`${BACKEND_URL}/api/cart/${productId}` ,{ amount }, {headers: { Authorization: `Bearer ${cookies.token}`}}); //calling api to save item in cart table
             
-            if(response.status === 200){
+            if(response.status === 200){ //handling 200 status code response
 
-                setInCartAmount(amount)
-                setAmount(0)
+                setInCartAmount(amount); //updating how many pieces of item we ordered
+                setAmount(0); //reseting input button for pieces
                 
-                setToggleAlert({status: true, type: "Success", statusCode: response.status, message: response.data.message});
+                setIsInCart(true); // updating isInCart state
+                
+                setToggleAlert({status: true, type: "Success", statusCode: response.status, message: response.data.message}); //toggling success message
+                setToggleAddToCart({status : false, product : null}); //untoggling AddToCart.jsx component
+                
+            };
 
-                setToggleAddToCart({status : false, product : null})
-            }
+        } catch (err) { //handling eeeors
 
-            setIsInCart(true)
-
-        } catch (err) {
-
-            if(err.status === 400){
-                setToggleAlert({status: true, type: "Failed", statusCode: err.status, message: String(err.response?.data?.message || err.message || 'Unknown error')});
-                setIsInCart(false)
-            setInCartAmount(0)
-            setAmount(amount)
-            }
-
-            setToggleAlert({status: true, type: "Internal_Error", statusCode: err.status, message: String(err.response?.data?.message || err.message || 'Unknown error')});
-            setIsInCart(false)
-            setInCartAmount(0)
-            setAmount(amount)
+            if(err.status === 400){ //handling 400 status code error
+                
+                setToggleAlert({status: true, type: "Failed", statusCode: err.status, message: String(err.response?.data?.message || err.message || 'Unknown error')});//toggling error message
             
-        }
-    }
+                setIsInCart(false); //updating state to false 
+            
+                setInCartAmount(0);
+                setAmount(amount); //reseting count states
+
+            };
+
+            // handling other status code errors
+
+            setToggleAlert({status: true, type: "Internal_Error", statusCode: err.status, message: String(err.response?.data?.message || err.message || 'Unknown error')}); //toggling internal error message
+            
+            setInCartAmount(0);
+            setAmount(amount);
+            setIsInCart(false); //reseting states
+            
+        };
+    };
 
     return(
         <div className="add-to-cart-container" style={{zIndex : 999}} key={toggleAddToCart.product.prodcuts_id} >
+
             <div className="add-to-cart-start d-flex align-items-center h-100">
                 <img src={`data:image/svg+xml;base64,${JSON.parse(toggleAddToCart.product.images)[0]}`}/>
             </div>
+
+
             <div className="add-to-cart-end w-100 h-100">
 
                 <div className="close-button d-flex justify-content-end">
@@ -59,11 +69,12 @@ const AddToCart = ({ setToggleAddToCart, toggleAddToCart, setToggleAlert }) => {
                       
                 <div className="product-main">
         
-                    <h2>{toggleAddToCart.product.title || <Skeleton count={1} width={'12vw'}/>}</h2>
+                    <h2>{toggleAddToCart.product.title}</h2>
                     <small className="text-break">{toggleAddToCart.product.description.length > 300 ? `${toggleAddToCart.product.description.slice(0,300)}...` : toggleAddToCart.product.description}</small>
-                    <h5 style={{backgroundColor : '#10b981', color : 'white', width : 'fit-content', fontSize : '14px'}} className="px-2 py-1 rounded-2">{`${toggleAddToCart.product.category} / ${toggleAddToCart.product.subcategory}` || <Skeleton count={2}/>}</h5>
+                    <h5 style={{backgroundColor : '#10b981', color : 'white', width : 'fit-content', fontSize : '14px'}} className="px-2 py-1 rounded-2">{`${toggleAddToCart.product.category} / ${toggleAddToCart.product.subcategory}`}</h5>
                     
                     <div className="product-amounts d-flex justify-content-between align-items-end">
+
                         <div className="amounts-start">
                             {!toggleAddToCart.product?.sales_price ? 
                                 <span className="d-flex align-items-center justify-content-start fw-bold" style={{fontSize : '20px', color : '#10b981'}}>${toggleAddToCart.product.price} </span> : 
@@ -71,8 +82,9 @@ const AddToCart = ({ setToggleAddToCart, toggleAddToCart, setToggleAlert }) => {
                                     <span style={{textDecoration: 'line-through', fontSize: '14px'}}>${toggleAddToCart.product.price} </span>
                                     <span className="d-flex align-items-center justify-content-start fw-bold" style={{fontSize : '20px', color : '#10b981'}}>${toggleAddToCart.product.sales_price} </span>
                                 </div>    
-                    }
+                            }
                         </div>
+
                         <div className="amounts-end">
                             <h5>{`Items Left: ${toggleAddToCart.product?.amount}`|| <Skeleton/>}</h5>
                         </div>
@@ -88,8 +100,8 @@ const AddToCart = ({ setToggleAddToCart, toggleAddToCart, setToggleAlert }) => {
                         </div>
                         <div className="row-end">
                             <button disabled={isInCart ? true : false} onClick={() => setAmount(prev => { if(prev - 1 <= 0) return 0;return prev - 1})}>-</button>
-                        <span>{isInCart ? inCartAmount : amount}</span>
-                        <button disabled={isInCart ? true : false} onClick={() => setAmount(prev => { if(prev + 1 > toggleAddToCart.product.amount)return prev; return prev + 1})}>+</button>
+                            <span>{isInCart ? inCartAmount : amount}</span>
+                            <button disabled={isInCart ? true : false} onClick={() => setAmount(prev => { if(prev + 1 > toggleAddToCart.product.amount)return prev; return prev + 1})}>+</button>
                         </div>
                     </div>
 
@@ -101,7 +113,9 @@ const AddToCart = ({ setToggleAddToCart, toggleAddToCart, setToggleAlert }) => {
                     <span>Free Shipping</span>
                 
                 </div>
+
             </div>
+
         </div>
     );
 };
