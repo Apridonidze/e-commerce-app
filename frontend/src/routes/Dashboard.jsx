276a0891afb960cc6a29603308c58535342ce8e8
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-04-06 18:42:25 +0400

    cleanup + doc : cleaned up and documenated Dashboard.jsx route

--- frontend/src/routes/Dashboard.jsx
+++ frontend/src/routes/Dashboard.jsx
@@ -1,80 +1,79 @@
-import { useContext, useEffect, useState } from "react"
-
-import User from "../component/User"
-import Header from '../layout/Header'
-import Sidebar from '../layout/Sidebar'
-import Cart from '../component/Cart'
-import CardDetails from '../component/CardDetails'
-
-import { useLocation } from 'react-router-dom'
-
-import { loadStripe } from "@stripe/stripe-js";
-import { Elements } from "@stripe/react-stripe-js"
-
-import { BACKEND_URL, STRIPE_PUBLIC_KEY } from '../../config'
-import { UserContext } from "../context/UserContext"
-import axios from "axios"
-import { useCookies } from "react-cookie"
-import CardHolder from "../component/CardHolder"
+import Header from '../layout/Header';
+import Sidebar from '../layout/Sidebar';
+import Footer from "../layout/Footer"
+import User from "../component/User";
+import Cart from '../component/Cart';
 import Order from "../component/Order"
 import OrderList from "../component/OrderList"
-import Footer from "../layout/Footer"
+import CardHolder from "../component/CardHolder"
+import StatusMessage from '../alerts/StatusMessage';
+import CardDetails from '../component/CardDetails'; //importing UI components
 
-const stripePromise = loadStripe(STRIPE_PUBLIC_KEY);
+import { useContext, useEffect, useState } from "react"; ///importign react hooks
 
+import { loadStripe } from "@stripe/stripe-js";
+import { Elements } from "@stripe/react-stripe-js"; //importing stripe components
+
+import axios from "axios";
+import { useLocation } from 'react-router-dom';
+import { useCookies } from "react-cookie"; //importing react libraries
+
+import { UserContext } from "../context/UserContext"; //importing  user context
+import { BACKEND_URL, STRIPE_PUBLIC_KEY } from '../../config'; //importing keys from config.jsx file
+
+const stripePromise = loadStripe(STRIPE_PUBLIC_KEY); //creating stripe promise
 
 
 const Dashboard = () => {
     
-    const { hash } = useLocation();
-
-    const [cookies] = useCookies(['token'])
-    const [toggleCard,setToggleCard] = useState(false)
-
-    const [toggleOrder,setToggleOrder] = useState(false)
-
-    const { user } = useContext(UserContext)
-    const { cardDetails } = useContext(UserContext)
-
-    const [cart , setCart] = useState([])
+    const { hash } = useLocation(); //defining hash from current url
     
+    const [ cookies ] = useCookies(['token']); //defining user cookies
+    const { user } = useContext(UserContext);
+    const { cardDetails } = useContext(UserContext); //defining user data from user context
+    
+    const [cart , setCart] = useState([]);//state to store cart items
+    
+    const [toggleCard,setToggleCard] = useState(false);
+    const [toggleOrder,setToggleOrder] = useState(false);
+    const [toggleAlert, setToggleAlert] = useState({status : false , type: '', statusCode : null, message : ''}); //states to toggle components
 
-        const [toggleAlert, setToggleAlert] = useState({status : false , type: '', statusCode : null, message : ''}); //states to toggle components
-
-    useEffect(() => {
-        if (hash) {
+    useEffect(() => {//scrolling user to sections if user chooses section from sidebar
         
-            const el = document.querySelector(hash);
-            if (el) {el.scrollIntoView({ behavior: "smooth" })}
+        if(!hash) return; //returning empty promise if hash is not defiend from url
+        
+        const el = document.querySelector(hash);//definign section based on given hash
+        if (el) {el.scrollIntoView({ behavior: "smooth" })}; //scrolling user to hash they provide
+        
+    }, [hash]);//triggering logic once hash is defined
 
-        }
-        return
-    }, [hash]);
 
-    const generateCustomerId = async () => {
+    const generateCustomerId = async () => { //function to generate customer intent for stripe payments
+
         try{
 
-            const GenerateCustomerId = await axios.post(`${BACKEND_URL}/api/stripe/create-customer-intent`, {email : user.email} , {headers : {Authorization : `Bearer ${cookies.token}`}})
-
-            if(GenerateCustomerId.status === 200){
-                setToggleCard(true)
-            } //toggle error + setToggleCard(false)
-            
+            const GenerateCustomerId = await axios.post(`${BACKEND_URL}/api/stripe/create-customer-intent`, {email : user.email} , {headers : {Authorization : `Bearer ${cookies.token}`}}); //making api call to stripe third party apis
+            if(GenerateCustomerId.status === 200)setToggleCard(true); //checking apis response and toggling card fillout component if customer internt in generated successfully and toggling fillout component
             
         }catch(err){
-            console.log(err)
-            // toggle alert message 
-            setToggleCard(false)
-        }
-    }
+            
+            setToggleAlert({status: true, type: "Internal_Error", statusCode: err.status, message: String(err.response?.data?.message || err.message || 'Unknown error')}); //toggling error message if customer intent could not be geneated
+            setToggleCard(false); //untoggling card details filloout component
+        
+        };
+    };
 
     useEffect(() => {
-    if (toggleOrder && !cardDetails?.last4) {
-        setToggleCard(true);
-        setToggleOrder(false);
-        // toggle alert message
-    }
-    }, [toggleOrder, cardDetails]);
+        
+        if (toggleOrder && !cardDetails?.last4) { //checking if user clicked on toggleOrder button and does not have card details filled out
+
+            setToggleCard(true); //toggling card fillout comonent
+            setToggleOrder(false); //disabling order component
+            setToggleAlert({status: true, type: "Warning", statusCode: 200, message: "First Fill Out Card Details To Place An Order."}); //triggering alert message to provide user with warning importmation
+        
+        }
+
+    }, [toggleOrder, cardDetails]); //triggering logic once these dependencies change
 
     return(
         <div className="main-container container-fluid d-flex flex-column justify-content-start">
