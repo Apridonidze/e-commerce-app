e730efc92402984e1053ae156e405e38fde1cc16
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-04-08 03:09:46 +0400

    cleanup + doc : cleaned up and documentated OrderList.jsx component

--- frontend/src/components/order/OrderList.jsx
+++ frontend/src/components/order/OrderList.jsx
@@ -1,68 +1,57 @@
-import axios from "axios"
-import { useEffect, useReducer, useRef, useState } from "react"
-import { BACKEND_URL } from "../../../config"
-import { useCookies } from "react-cookie"
-import OrderDetails from "./OrderDetails"
+import axios from "axios";
+import { useCookies } from "react-cookie"; ///importing react libraries
+
+import { useEffect, useRef, useState } from "react"; //importing react hooks
+import { BACKEND_URL } from "../../../config"; //importing backend url from config file
+
+import OrderDetails from "./OrderDetails"; //importing react components
 
 const OrderList = () => {
 
-    const [cookies] = useCookies(['token'])
+    const [cookies] = useCookies(['token']); //defining user cookies
 
-    const [orders,setOrders] = useState([])
-    const [type , setType] = useState('Progress')
+    const [orders,setOrders] = useState([]); //state to store users orders
+    const [type , setType] = useState('Progress'); //state to store type of orders user want to see
 
     const btn1Ref = useRef(null);
-    const btn2Ref = useRef(null);
+    const btn2Ref = useRef(null);//refs for oders type button
 
     useEffect(() => {
 
         const fetchOrders = async() => {
-
             try{
 
-                const orders = await axios.get(`${BACKEND_URL}/api/order`, {headers : {Authorization : `Bearer ${cookies.token}`}})
+                const orders = await axios.get(`${BACKEND_URL}/api/order`, {headers : {Authorization : `Bearer ${cookies.token}`}}); //making api call
                 
-                if(orders.status === 204) setOrders([])
-                
-                    console.log(orders)
-                if(orders.status === 200){
-                    setOrders(orders.data.orders)
-                }
+                if(orders.status === 204) return setOrders([]); //handling 204 status code
+                if(orders.status === 200) return setOrders(orders.data.orders); //handling 200 staus code
 
             }catch(err){
-                console.log(err)
-            }
+               // add alert messages
+            };
+        };
 
-        }
+        fetchOrders(); //declearing function
 
-        return () => fetchOrders()
+    },[]); //logic triggers on component mount
 
-    },[])
 
     const handleToggleOrder = (e) => {
 
-        e.preventDefault();
+        e.preventDefault(); //handling page reload whne function in tirggered
 
-        if(btn1Ref && btn1Ref.current && btn2Ref && btn2Ref.current){
+        if(btn1Ref && btn1Ref.current && btn2Ref && btn2Ref.current){ //checking if refs are defined and executing logic
             
-            const buttons = [btn1Ref.current , btn2Ref.current];
+            const btn = e.currentTarget; //defining target button
+            const buttons = [btn1Ref.current , btn2Ref.current]; //defining button refs
 
-            const btn = e.currentTarget
-            btn.classList.add('active')
-            setType(btn.id)
+            btn.classList.add('active'); //adding active state to target button
+            setType(btn.id); //storing type of ordesr user clicks to see
             
-            buttons.forEach((untargetedBtn) => {
-                if(!untargetedBtn) return;
-
-                if(untargetedBtn !== btn){
-                    untargetedBtn.classList.remove('active')
-                }
-            })
-
-        }
-
-
-    }
+            buttons?.map((nonTarget) => nonTarget !== btn ? nonTarget.classList.remove('active') : nonTarget.classList.add('active')); //mapping over buttons refs and styling them based on if they are targeted or not
+            
+        };
+    };
 
     return(
         <div className="order-list-container">
@@ -73,7 +62,7 @@ const OrderList = () => {
                     <h6>Manage your orders</h6>
                 </div>
                 <div className="order-list-end">
-                    <button className="btn border-0" id="Progress" onClick={(e) => handleToggleOrder(e)} ref={btn1Ref}>My Order</button>
+                    <button className="btn border-0 active" id="Progress" onClick={(e) => handleToggleOrder(e)} ref={btn1Ref}>My Order</button>
                     <button className="btn border-0" id="Delivered" onClick={(e) => handleToggleOrder(e)} ref={btn2Ref}>Delivered</button>
                 </div>
             </div>
@@ -84,7 +73,7 @@ const OrderList = () => {
             )) : `no ${type}`}
 
         </div>
-    )
-}
+    );
+};
 
-export default OrderList
+export default OrderList; //exporting component
