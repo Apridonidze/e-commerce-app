43bdf63926c0d3e915d3ca0967480f44a99d67eb
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-04-07 20:59:00 +0400

    style : styled Cart.jsx component responsiveness

--- frontend/src/component/EmptyCart.jsx
+++ frontend/src/component/EmptyCart.jsx
@@ -1,16 +1,15 @@
 const EmptyCart = () => {
-    return(
-        <div className="empty-cart-container d-flex flex-column align-items-center text-center p-3 ">
-            
-            <div class="icon-bg my-2"><i class="fa-solid fa-cart-shopping"></i></div>
-
-            <h4>Your Cart Is Empty</h4>
-            <small>Browse products and add them to your cart.</small>
-
-            <button className="buttonComponent mt-4 btn btn-none text-white px-2 py-2"><i class="fa-regular fa-compass"></i> Explore</button>
-
+    return (
+        <div className="empty-cart-container w-100">
+            <div className="empty-cart-wrapper d-flex flex-column align-items-center text-center w-100 p-3">
+                <div className="icon-bg my-2"><i className="fa-solid fa-cart-shopping"></i></div>
+                <h4>Your Cart Is Empty</h4>
+                <small>Browse products and add them to your cart.</small>
+                <button className="buttonComponent mt-4 btn btn-none text-white px-2 py-2">
+                    <i className="fa-regular fa-compass"></i> Explore
+                </button>
+            </div>
         </div>
     );
 };
-
 export default EmptyCart; //exporting component
