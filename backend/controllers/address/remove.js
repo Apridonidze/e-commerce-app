386a01406b5552bd85018f755477877e857fb2e9
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-04-09 06:37:57 +0400

    feat: developed remove.js service in address controllers

--- backend/controllers/address/remove.js
+++ backend/controllers/address/remove.js
@@ -1,5 +1,21 @@
-function remove (){
+const db = require("../../utils/db");
 
+async function remove (req,res){
+
+    if(!Number(req.query.id)) return res.status(400).json({message : "Invalid Address Id"}) 
+
+    try{
+
+        const { id } = req.query;
+
+        const [ row ] = await db.query('delete from address where id = ?' , [ id ]);
+        if(row.affectedRows === 0) return res.status(400).json({message : "Address Id Not Found"})
+
+        return res.status(200).json({message : "Address Removed Succesfully"})
+
+    }catch(err){
+        return res.status(500).json({message : "Could Not Remove Your Address. Try Later!"})
+    }
 }
 
 module.exports = remove;
