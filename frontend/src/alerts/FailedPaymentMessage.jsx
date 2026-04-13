65932fd5096d34d0de6110db12061587c85aa672
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-04-13 23:13:29 +0400

    feat: added ocntent to Failed/Success PaymentMessage.jsx components

--- frontend/src/alerts/FailedPaymentMessage.jsx
+++ frontend/src/alerts/FailedPaymentMessage.jsx
@@ -1,8 +1,20 @@
 const FailedPaymentMessage = () => {
     return(
         <div className="payment-message-container d-flex align-items-center justify-content-start gap-3 h-auto">
-            Payment Message
-            
+            <i class="fa-regular fa-circle-xmark"></i>
+
+            <h3>Payment Failed</h3>
+            <h6>Your payment has been processed successfully. Your order is now confirmed and being prepared.</h6>
+
+            <div className="payment-row">
+                <h6>We couldn’t process your payment due to a technical issue, declined transaction, failed authentication, insufficient funds, expired session, or network error, and no charges were made to your account, so please check your details, try again, or use a different payment method, and contact support if the issue persists.</h6>
+            </div>
+
+            <div className="d-flex">
+                <button>Try Again</button>
+                <button>Return To Dashboard</button>
+            </div>
+
         </div>
     )
 }
--- frontend/src/alerts/SuccessPaymentMessage.jsx
+++ frontend/src/alerts/SuccessPaymentMessage.jsx
@@ -3,10 +3,22 @@ const PaymentMesSuccessPaymentMessagesage = () => {
 
     return(
         <div className="payment-message-container d-flex align-items-center justify-content-start gap-3 h-auto">
-            Payment Message
+            <i class="fa-regular fa-circle-check"></i>
             
+            <h3>Payment Successful</h3>
+            <h6>Your payment has been processed successfully. Your order is now confirmed and being prepared.</h6>
+
+            <div className="payment-row">
+                <h6>Order Id : </h6>
+                <h6>Excepted Delivery : </h6>
+            </div>
+
+            <div className="d-flex">
+                <button>Return To Dashboard</button>
+            </div>
+
         </div>
     )
 }
 
-export default SuccessPaymentMessage
+export default PaymentMesSuccessPaymentMessagesage
