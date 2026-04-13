2e7c239626613d539c0ed80550410b0e4bb0301e
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-04-13 20:42:53 +0400

    fix : fixed users orders list data structure in list.js

--- backend/controllers/order/list.js
+++ backend/controllers/order/list.js
@@ -3,7 +3,7 @@ const db = require('../../utils/db'); //importing db utility
 async function list(req,res) {
     try{
 
-        const [orders] = await db.query("select orders.* , ordered_items.product_id, ordered_items.amount, ordered_items.price from orders  join ordered_items on orders.order_id = ordered_items.order_id join address on orders.address = address.id where orders.user_id = ?" , [req.user.userId]); //fetching order items with joins with products table 
+        const [orders] = await db.query("select orders.*, address.* from orders join address on orders.address = address.id where orders.user_id = ?" , [req.user.userId]); //fetching order items with joins with products table 
         
         if(orders.length === 0) return res.status(204).send(); //sending 204 status code if order.length === 0
         return res.status(200).json({message : 'Orders Fetched Successfully' , orders}); //sending 200 status code if orders are found
