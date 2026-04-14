2036b3dab58eddecdaa832c2eebd5e07c2fc757b
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-04-15 03:57:05 +0400

    style : satyled OrderDetails.jsx with OrderItems.jsx component + added responsive design for it

--- frontend/src/components/order/OrderDetails.jsx
+++ frontend/src/components/order/OrderDetails.jsx
@@ -42,37 +42,40 @@ const OrderDetails = ({order, orderId, key , setOrders}) => {
         }
     }
 
-    return(
-        <div className="order-details-container" key={orderId}>
+    const [openId, setOpenId] = useState(null);
 
-            <div className="order-top border d-flex justify-content-between">
+    return(
+        <div className="order-details-container rounded-3 " key={orderId}>
+
+            <div className="order-top d-flex align-items-center p-3 justify-content-between">
                 <div className="order-start d-flex gap-2">
                     
                     <div className="order-icon">{order.status == 'Pending' ? <i class="fa-regular fa-truck"></i> : order.status == 'OnWay' ? <i class="fa-solid fa-clipboard-check"></i> : <i class="fa-solid fa-envelope-circle-check"></i>}</div>
                     
                     <div className="d">
-                        <div className="d-flex">
-                            <h4>Order #{order.order_id}</h4>
-                            <span className={`order ${order.status}`}> {order.status}</span>
+                        <div className="d-flex gap-3">
+                            <h4 className="fw-bold">Order #{order.order_id}</h4>
+                            <span className={`order ${order.status} rounded-1 py-1`}> {order.status}</span>
                         </div>
                         <h6>Expected Delivery: {new Date(new Date(order.created_at).getTime() + 7 * 24 * 60 * 60 * 1000).toLocaleDateString()}</h6>
                     </div>
 
                 </div>
-                <div className="order-end border h-100 d-flex flex-column align-items-end border">
-                    <button className="btn btn-primary" onClick={() => setToggleDrop(!toggleDrop)}>:</button>
-                    <div className="toggle text-white" style={{ display : toggleDrop ? 'flex' : 'none' , flexDirection : 'column',position : 'relative', top : '10px'}}>
-                        <button className="btn btn-danger" disabled={order.status === 'OnWay' || order.status === 'Delivered' ? true : false}  onClick={() => discardOrder(order.order_id)}>Discard Order</button>
+                <div className="order-end h-100 d-flex align-items-center gap-3 ">
+                    <div className="d-flex flex-column">
+                        <span style={{letterSpacing : '1px'}} className="fw-medium text-secondary">TOTAL VALUE</span><br />
+                        <span className="fw-bold">${order.total_price.toFixed(2)}</span>
                     </div>
                     <div className="d d-flex justify-content-between">
-                    <button className="btn btn-primary" onClick={() => {products.length == [] ? fetchOrderDetails(order.order_id) : ''}} type="button" data-toggle="collapse" data-target={`#collapseDiv${orderId}`} aria-expanded="false" aria-controls={`collapseDiv${orderId}`}>^</button>
-                </div>
+                        <button className="btn fw-bold border-0 btn-none" onClick={() => {if (products.length === 0) fetchOrderDetails(order.order_id); setOpenId(prev => (prev === orderId ? null : orderId));}} type="button" data-toggle="collapse" data-target={`#collapseDiv${orderId}`} aria-expanded="false" aria-controls={`collapseDiv${orderId}`}><i className={`fa-solid fa-angle-right ${openId === orderId ? 'rotate' : ''}`}></i></button>
+                    </div>
                 </div>
             </div>
 
-            <div className="order-bottom">
-                
-                <div className="collapse" id={`collapseDiv${orderId}`}>
+            <div className="order-bottom p-0">
+                <div className="collapse w-100 p-0" id={`collapseDiv${orderId}`}>
+
+                    <h6 className="px-3 pt-3">Ordered Items</h6>
                     {products?.map((prod, prodId) => <OrderItem prod={prod} prodId={prodId} key={prodId} />)}
                 </div>
             </div>
--- frontend/src/components/order/OrderItem.jsx
+++ frontend/src/components/order/OrderItem.jsx
@@ -1,11 +1,11 @@
 const OrderItem = ({ prod,prodId, key }) => {
     return(
-        <div className="order-item container d-flex justify-content-between" key={prodId}>
-            <div className="item-start d-flex">
-                {<img className="w-100 h-100 rounded-1" src={`data:image/svg+xml;base64,${JSON.parse(prod?.images)[0]}`} style={{maxHeight:'80px' , maxWidth : '80px'}}/>}
+        <div className="order-item p-3 d-flex gap-2 align-items-center justify-content-between" key={prodId}>
+            <div className="order-row d-flex gap-3">
+                {<img className="rounded-1" src={`data:image/svg+xml;base64,${JSON.parse(prod?.images)[0]}`} style={{maxHeight:'80px' , maxWidth : '80px'}}/>}
                 <div className="item-details">
                     <h5>{prod?.title}</h5>
-                    <small>{prod.description?.length < 40 ? `${prod?.description.slice(0,40)}...` : prod?.description}</small>
+                    <small>{prod.description?.length < 35 ? `${prod?.description.slice(0,35)}...` : prod?.description}</small>
                 </div>
             </div>
         </div>
