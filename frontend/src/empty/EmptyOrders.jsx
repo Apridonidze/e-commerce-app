5ef4b83fdb211d231606eaaefc7411ecb1d92cfc
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-04-14 16:02:11 +0400

    feat: added content to EmptrOrder.jsx component

--- frontend/src/empty/EmptyOrders.jsx
+++ frontend/src/empty/EmptyOrders.jsx
@@ -1,7 +1,9 @@
 const EmptyOrders = () => {
     return(
         <div className="empty-orders-container">
-
+            <div className="empty-icon"><i class="fa-solid fa-clock-rotate-left"></i></div>
+            <h3>You don’t have any orders yet   </h3>
+            <h6>Once you place an order, it will appear here and move to delivered after completion.</h6>
         </div>
     );
 };
