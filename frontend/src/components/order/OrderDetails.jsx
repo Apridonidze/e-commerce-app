7f47267a681b60b5a03d153d230fba8e769eef98
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-04-15 06:10:42 +0400

    cleanup + doc : cleanued up and documenatted OrderDetail;s.jsx component

--- frontend/src/components/order/OrderDetails.jsx
+++ frontend/src/components/order/OrderDetails.jsx
@@ -1,39 +1,40 @@
 import axios from "axios";
-import { useCookies } from "react-cookie";
+import { useCookies } from "react-cookie"; //importing react libraries
 
-import { useState } from "react";
+import { useState } from "react"; //importing react hook
+import { BACKEND_URL } from "../../../config"; //importing backend url from config file
 
-import OrderItem from "./OrderItem";
-import { BACKEND_URL } from "../../../config";
-import ItemSkeleton from "../../skeletons/ItemSkeleton";
-import SmallItemSkeleton from "../../skeletons/SmallItemSkeleton";
-
+import OrderItem from "./OrderItem"; //importing react component
+import SmallItemSkeleton from "../../skeletons/SmallItemSkeleton"; //importing loading skeleton
 
 const OrderDetails = ({ order, setToggleAlert }) => {
 
-    const [ cookies ] = useCookies(['token'])
+    const [ cookies ] = useCookies(['token']); //definin user cookies
 
-    const [products,setProducts] = useState([]);
-    const [openId, setOpenId] = useState(null);
+    const [products,setProducts] = useState([]); //state to store order's product list
+    const [openId, setOpenId] = useState(null); //state to trigger collapse button animation
 
-    const [isLoading , setIsLoading] = useState(true);
+    const [isLoading , setIsLoading] = useState(true); //state to trigger loading skeleton
 
     const fetchOrderDetails = async(id) => {
         try{
 
             const response = await axios.get(`${BACKEND_URL}/api/order/${id}` , {headers: {Authorization : `Bearer ${cookies.token}`}}); //making api call
 
-            setProducts(response.data.orderItems);
-            setIsLoading(false);
+            setProducts(response.data.orderItems); //storing data in state
+            setIsLoading(false); //storing false state to disable loading skeleton
             
         }catch(err){
-            if(err.status == 404){
-                setProducts([]);
-            setIsLoading(false);
-            }
-            setProducts([]);
-            setIsLoading(false);
-            return setToggleAlert()
+            if(err.status == 404){ //handling 404 status code error
+                setProducts([]); //setting empty array as products
+                setIsLoading(false); //storing false state to disable loading skeleton
+                return setToggleAlert({status: true, type: "Failed", statusCode: err.status, message: String(err.response?.data?.message || err.message || 'Unknown error')}); //toggling error message if order details could not be fetched because of users provided order id
+            };
+
+            setProducts([]); //storing empty arrray as products
+            setIsLoading(false); //storing false state to disable loading skeleton
+            return setToggleAlert({status: true, type: "Internal Error", statusCode: err.status, message: String(err.response?.data?.message || err.message || 'Unknown error')}); //toggling error message if order details could not be fetched because of server error
+
         };
     };
     
@@ -43,7 +44,9 @@ const OrderDetails = ({ order, setToggleAlert }) => {
             <div className="order-top d-flex align-items-center p-3 justify-content-between">
                 <div className="order-start d-flex gap-2">
                     
-                    <div className="order-icon">{order.status == 'Pending' ? <i class="fa-regular fa-truck"></i> : order.status == 'OnWay' ? <i class="fa-solid fa-clipboard-check"></i> : <i class="fa-solid fa-envelope-circle-check"></i>}</div>
+                    <div className="order-icon">
+                        {order.status == 'Pending' ? <i class="fa-regular fa-truck"></i> : order.status == 'OnWay' ? <i class="fa-solid fa-clipboard-check"></i> : <i class="fa-solid fa-envelope-circle-check"></i>}
+                    </div>
                     
                     <div className="d">
                         <div className="d-flex gap-3">
