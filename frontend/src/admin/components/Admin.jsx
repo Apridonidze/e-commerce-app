923c8af0bc1166fa2b5fbfe8455ce3cd2e4c7c40
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-04-16 18:52:04 +0400

    style : styled AdminList.jsx component

--- frontend/src/admin/components/Admin.jsx
+++ frontend/src/admin/components/Admin.jsx
@@ -1,7 +1,12 @@
-const Admin = () => {
+const Admin = ({ admin, status }) => {
     return(
-        <div className="admin-container">
-            
+        <div className="admin-container text-center rounded-3" key={admin.id}>
+            <div className="admin-icon">
+                <i class="fa-regular fa-user border mx-auto border-2 border-dark rounded-5 px-4 py-2 d-flex align-items-center justify-content-center" style={{fontSize: "32px", border : 'none'}}></i>
+                <span className="activity" style={{ width: '10px', height: '10px', borderRadius: '50%', display: 'inline-block', backgroundColor: status ? '#10b981' : '#9ca3af'}}></span>  
+            </div>
+            <h6>{admin.fullname}</h6>
+            <span style={{color : status ? '#10b981' : ''}}>{status ? 'Online' : 'Offline'}</span>
         </div>
     )
 }
--- frontend/src/admin/components/AdminList.jsx
+++ frontend/src/admin/components/AdminList.jsx
@@ -1,24 +1,20 @@
+import Admin from "./Admin"
+
 const AdminList = ({ admins, setToggleManageAdmins }) => {
 
     let adminList = [...admins.onlineAdmins , ...admins.offlineAdmins].slice(0, 5)
     
     return(
-        <div className="admin-list-container">
-            <div className="header d-flex">
-                <span>Admin Squad</span>
-                <button onClick={() => setToggleManageAdmins(true)}>Manage Admins</button>
+        <div className="admin-list-container p-2">
+            
+            <div className="admin-header d-flex justify-content-between ms-3 mt-2">
+                <h4> <i class="fa-solid fa-user-group me-2"></i> Admin Squad</h4>
+                <button onClick={() => setToggleManageAdmins(true)}>Manage Admins <i class="fa-solid fa-arrow-right"></i></button>
             </div>
 
-            <div className="admin-div">
-
-                {adminList.length ? adminList.map((admin, adminId) => 
-                    <div className="admin d-flex justify-content-between" key={adminId}>
-                        <div className="admin-start">
-                            <span>Fullname : {admin.fullname}</span>
-                            <span>Id : {admin.id}</span>
-                            <span>Status: {admins.onlineAdmins.some(adm => adm.id === admin.id) ? 'online' : 'offline'}</span>
-                        </div>
-                    </div>
+            <div className="row gap-3 m-3">
+                {adminList.length ? adminList.map(admin => 
+                    <Admin admin={admin} status={admins.onlineAdmins.some(adm => adm.id === admin.id) ? true : false}/>
                 ) : 'No Admins Online'}
             </div>
 
