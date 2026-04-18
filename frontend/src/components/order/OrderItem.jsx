14dad41b58e55e134f63427a1dd075c3e411e993
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-04-19 01:59:45 +0400

    feat: added urls to OrderDetails.jsx component

--- frontend/src/components/order/OrderItem.jsx
+++ frontend/src/components/order/OrderItem.jsx
@@ -1,10 +1,17 @@
+import { Link } from "react-router-dom";
+
 const OrderItem = ({ prod }) => {
     return(
         <div className="order-item p-3 d-flex gap-2 align-items-center justify-content-between" key={prod.product_id}>
             <div className="order-row d-flex gap-3">
-                <img className="rounded-1 w-100 h-auto" src={`data:image/svg+xml;base64,${JSON.parse(prod?.images)[0]}`} style={{maxHeight:'80px' , maxWidth : '80px'}}/>
+                <Link to={`/product/${prod.product_id}`}>
+                    <img className="rounded-1 w-100 h-auto" src={`data:image/svg+xml;base64,${JSON.parse(prod?.images)[0]}`} style={{maxHeight:'80px' , maxWidth : '80px'}}/>
+                </Link>
                 <div className="item-details">
-                    <h5>{prod.title}</h5>
+                    <Link to={`/product/${prod.product_id}`}>
+                        <h5>{prod.title}</h5>
+                    </Link>
+
                     <small>{prod.description.length < 35 ? `${prod?.description.slice(0,35)}...` : prod?.description}</small>
                 </div>
             </div>
