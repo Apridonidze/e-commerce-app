ce372c6f0ea1dc308fa279a1a4c88992d8924a60
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-04-20 04:39:23 +0400

    style : adjusted minor style commands for Report.jsx component

--- frontend/src/components/report/Report.jsx
+++ frontend/src/components/report/Report.jsx
@@ -1,20 +1,28 @@
 import { Link } from "react-router-dom";
 
 const Report = ( { report, setToggleDeleteReport, setToggleRespondReport } ) => {
-    console.log(report)
     return(
         <div className="report-container d-flex flex-column rounded-3 p-2" key={report.report_id}>
             
             <div className="report-top d-flex justify-content-between py-3">
 
-                <div className="report-user-initials">
-                    <span className="userInitials text-uppercase me-2">{report.user_fullname.split(' ')[0].at(0)}{report.user_fullname.split(' ')[1].at(0)}</span>
-                    <span className="fs-5 fw-bold">{report.user_fullname}</span>
+                <div className="report-user-initials w-auto">
+
+                    <div className="initials"><span className="userInitials text-uppercase me-2">{report.user_fullname.split(' ')[0].at(0)}{report.user_fullname.split(' ')[1].at(0)}</span>
+                    <span className="fs-5 fw-bold">{report.user_fullname}</span></div>
+
+                    <div className="toggle-report-button gap-1">
+                        <button className="replyBtn btn border-0 py-2" onClick={() => setToggleRespondReport({status : true, reportDetails : report})} disabled={report.status == 'Removed' || report.status == 'Responded' ? true : false } ><i class="fa-solid fa-reply"></i></button> 
+                        <button className="deleteBtn btn border-0 py-2" onClick={() => setToggleDeleteReport({status : true, reportDetails : report})} disabled={report.status == 'Removed' ? true : false}><i class="fa-solid fa-trash-can"></i></button>
+                    </div>
+
                 </div>
 
-                <span>{report.user_email}</span>
-                <span>{report.type}</span>
-                <span>{report.status}</span>
+                <div className="d-flex align-items-center w-50 gap-5 justify-content-between me-3">
+                    <span>{report.user_email}</span>
+                    <span className="type px-3 fs-6 rounded-3">{report.type}</span>
+                    <span className={`status ${report.status}`}><i class="statusDot fa-solid fa-circle"></i> {report.status}</span>
+                </div>
                 
                 <div className="report-end d-flex gap-1">
                     <button className="replyBtn btn border-0 py-2" onClick={() => setToggleRespondReport({status : true, reportDetails : report})} disabled={report.status == 'Removed' || report.status == 'Responded' ? true : false } ><i class="fa-solid fa-reply"></i></button> 
