7eb62c2d29affee87855aa20e20fe1a862486533
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-04-20 12:14:46 +0400

    mod : modified charts.js service to return data in one month period of time for admin charts

--- backend/controllers/dashboard/charts.js
+++ backend/controllers/dashboard/charts.js
@@ -3,7 +3,7 @@ const db = require('../../utils/db')
 async function charts(req,res){
     try{
 
-        const [ soldItems ] = await db.query(`select ordered_items.product_id, ordered_items.amount, orders.created_at, products.title, products.category, products.subcategory, products.price, products.sales_price from ordered_items join orders on ordered_items.order_id = orders.order_id join products on products.products_id = ordered_items.product_id order by orders.created_at;`); //selecting sold items to generate charts data for admin dashobard
+        const [ soldItems ] = await db.query(`select ordered_items.product_id, ordered_items.amount, orders.created_at, products.title, products.category, products.subcategory, products.price, products.sales_price from ordered_items join orders on ordered_items.order_id = orders.order_id join products on products.products_id = ordered_items.product_id where orders.created_at between date_sub(now(), interval 1 month) and now() order by orders.created_at desc`); //selecting sold items to generate charts data for admin dashobard
 
         const getRevenue = (curr) => curr.amount * (curr.sales_price ?? curr.price);
 
@@ -88,6 +88,7 @@ async function charts(req,res){
         return res.status(200).json({salesOverTime, bestSellingProducts, revenueByProduct, categoryDistribution, topProducts})
 
     }catch(err){
+        console.log(err)
         return res.status(500).json({message : "Could Not Load Dashboard Information. Try Later"}); //returning internal error message
     }
 }
