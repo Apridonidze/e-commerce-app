247a518236593aa175a583465df146606a030c63
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-04-22 04:17:19 +0400

    style : styled Stats.jsx component

--- frontend/src/components/analytics/Stats.jsx
+++ frontend/src/components/analytics/Stats.jsx
@@ -1,8 +1,20 @@
 const Stats = ({ chartsData }) => {
-    console.log(chartsData)
+
+    const displayData = [
+        {id : 0 , icon : <i class="fa-solid fa-money-bills"></i> , title : "TOTAL REVENUE" , value : Number(chartsData.totalRevenue).toFixed(2)},
+        {id : 1 , icon : <i class="fa-solid fa-bag-shopping"></i> , title : "TOTAL ORDERS" , value : chartsData.totalOrders},
+        {id : 2 , icon : <i class="fa-solid fa-user-group"></i> , title : "TOTAL USERS" , value : chartsData.totalUsers},
+    ]
+
     return(
-        <div className="stats-container">
-            
+        <div className="stats-container mt-4">
+            {displayData.map(data => 
+                <div className="data-container d-flex flex-column p-3 rounded-3" key={data.id}>
+                    {data.icon}
+                    <h6>{data.title}</h6>
+                    <span>{data.value}</span>
+                </div>
+            )}
         </div>
     )
 }
