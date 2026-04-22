0bd2ba53d6b748934d06d4f303ba0724871047b2
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-04-22 12:32:57 +0400

    style : styled EmptyAdmin.jsx component

--- frontend/src/empty/EmptyAdmin.jsx
+++ frontend/src/empty/EmptyAdmin.jsx
@@ -1,7 +1,7 @@
 const EmptyAdmin = () => {
     return(
-        <div className="empty-admin-container text-center rounded-3" style={{minHeight : "220px" , maxWidth : '100%'}}>
-            <i class="fa-solid fa-plane-slash"></i>
+        <div className="empty-admin-container d-flex flex-column justify-content-center text-center rounded-3" style={{minHeight : "220px" , maxWidth : '100%'}}>
+            <i className="fa-solid fa-plane-slash w-auto p-3 rounded-4 mb-3"></i>
             <h4>Could Not Fetch Active Admin's List</h4>
             <h6>Try Later!</h6>
         </div>
