d1591e994d270eac5c2aeadc78466bd0bc845d25
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-04-22 16:31:56 +0400

    doc + cleanup : documentated and lceaned up ManageOrders.jsx component

--- frontend/src/admin/components/orders/ManageOrders.jsx
+++ frontend/src/admin/components/orders/ManageOrders.jsx
@@ -1,55 +1,55 @@
 import axios from "axios";
-import OrderBox from "./OrderBox";
-
-import { BACKEND_URL } from "../../../../config";
 import { useCookies } from "react-cookie";
+import { Link } from "react-router-dom"; //importing react libraries
 
-import { Link } from "react-router-dom";
-import EmptyOrders from "../../../empty/EmptyOrders";
+import OrderBox from "./OrderBox";
+import EmptyOrders from "../../../empty/EmptyOrders"; //importing react componnets
 
-const ManageOrders = ({ orders, setOrders, setToggleAlert }) => {
+import { BACKEND_URL } from "../../../../config"; //importing backend url from config file
+ 
+const ManageOrders = ({ orders, setOrders, setToggleAlert }) => { //importing props from parent component (AdminDashboard.jsx)
 
-    const [ cookies ] = useCookies(['token'])
-    const orderStatuses = ['Pending' , 'OnWay' , 'Delivered'];
+    const [ cookies ] = useCookies(['token']); //cookies
+    const orderStatuses = ['Pending' , 'OnWay' , 'Delivered']; //array of allowed order statuses
 
-    const handleStatusChange = async(id, status) => {
+    const handleStatusChange = async(id, status) => {//api function that recieved id and status of order and changes status via api
+
+        if(!orderStatuses.includes(status)) return;// returning empty promise if provided status is not in allowed array's list
+
         try{
 
-            const response = await axios.put(`${BACKEND_URL}/api/dashboard/${id}` , {status}, {headers : {Authorization : `Bearer ${cookies.token}`}})
-            if(response.status === 200){
-                setOrders(prevOrders => prevOrders?.map(ord => ord.order_id === id ? { ...ord, status: response.data.status } : ord))
-            }
+            const response = await axios.put(`${BACKEND_URL}/api/dashboard/${id}` , {status}, {headers : {Authorization : `Bearer ${cookies.token}`}}); //making api call
+            if(response.status === 200) setOrders(prevOrders => prevOrders?.map(ord => ord.order_id === id ? { ...ord, status: response.data.status } : ord)); //handlign 200 status code by instantly updating order in state
 
-        }catch(err){
-            if(err.status === 400) return setToggleAlert({status: true, type: "Warning", statusCode: err.status, message: String(err.response?.data?.message || err.message || 'Unknown error')});
-            if(err.status === 404)return setToggleAlert({status: true, type: "Warning", statusCode: err.status, message: String(err.response?.data?.message || err.message || 'Unknown error')});
-            return setToggleAlert({status: true, type: "Internal_Error", statusCode: err.status, message: String(err.response?.data?.message || err.message || 'Unknown error')});
-        }
-    }
+        }catch(err){ //catching errors
+            if(err.status === 400) return setToggleAlert({status: true, type: "Failed", statusCode: err.status, message: String(err.response?.data?.message || err.message || 'Unknown error')}); //handligng 400 status code errror (if user injects invalid params)
+            if(err.status === 404)return setToggleAlert({status: true, type: "Warning", statusCode: err.status, message: String(err.response?.data?.message || err.message || 'Unknown error')}); //handling 404 status code (if order is not found in database)
+            return setToggleAlert({status: true, type: "Internal_Error", statusCode: err.status, message: String(err.response?.data?.message || err.message || 'Unknown error')});//handling other type of internal errors (500 status code eeeors)
+        };
+    };
 
-
-    const removeOrder = async(id) => {
+    const removeOrder = async(id) => { //recieving id param
         try{
 
-            const response = await axios.delete(`${BACKEND_URL}/api/order/admin-remove/${id}` , {headers: {Authorization : `Bearer ${cookies.token}`}})
-
-            if(response.status === 200)return setOrders(prev => prev.filter(ord => ord.order_id !== id))
-            setToggleDrop(false)
+            const response = await axios.delete(`${BACKEND_URL}/api/order/admin-remove/${id}` , {headers: {Authorization : `Bearer ${cookies.token}`}}); //m,making api call
+            
+            if(response.status === 200) return setOrders(prev => prev.filter(ord => ord.order_id !== id)); //handling 200 status code
+            setToggleDrop(false); //untoggling select form
 
         }catch(err){
-            if(err.status === 400) return setToggleAlert({status: true, type: "Warning", statusCode: err.status, message: String(err.response?.data?.message || err.message || 'Unknown error')});
-            if(err.status === 404)return setToggleAlert({status: true, type: "Warning", statusCode: err.status, message: String(err.response?.data?.message || err.message || 'Unknown error')});
-            return setToggleAlert({status: true, type: "Internal_Error", statusCode: err.status, message: String(err.response?.data?.message || err.message || 'Unknown error')});
-        }
-    }
+            if(err.status === 400) return setToggleAlert({status: true, type: "Failed", statusCode: err.status, message: String(err.response?.data?.message || err.message || 'Unknown error')}); //handling 400 status code (invalid_params)
+            if(err.status === 404)return setToggleAlert({status: true, type: "Warning", statusCode: err.status, message: String(err.response?.data?.message || err.message || 'Unknown error')}); //handling 404 status code (order not found)
+            return setToggleAlert({status: true, type: "Internal_Error", statusCode: err.status, message: String(err.response?.data?.message || err.message || 'Unknown error')}); //handlking other types of errors (internal errors)
+        };
+    };
 
     return (
-        <div className="manage-orders-container mt-5">
-            <div className="manage-orders-header"><h4><i className="fa-solid fa-box-open me-2" style={{color : "#10b981"}} ></i> Orders</h4></div>
+        <div className="manage-orders-container mt-5 mx-2">
+
+            <div className="manage-orders-header my-3"><h4><i className="fa-solid fa-box-open me-2" style={{color : "#10b981"}} ></i> Orders</h4></div>
             <div className="orders-container">
 
                 {orderStatuses?.map((status => (    
-
                     <div className={`default-container ${status} p-2 rounded-3`}>
                         
                         <div className="manage-order-header d-flex justify-content-between mb-3 align-items-center">
@@ -60,12 +60,12 @@ const ManageOrders = ({ orders, setOrders, setToggleAlert }) => {
                         {orders?.filter(ord => ord.status == status).length > 0 ? orders?.filter(ord => ord.status == status).map(order => <OrderBox order={order} orderStatuses={orderStatuses} handleStatusChange={handleStatusChange} removeOrder={removeOrder}/>) : <EmptyOrders status={status}/>}
 
                     </div>
-
                 )))}
 
             </div>
+
         </div>
     );
 };
 
-export default ManageOrders ; 
+export default ManageOrders;//exporting component
