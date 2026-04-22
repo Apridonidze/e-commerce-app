0533ecf99bc2af4987f5c489aed5b586d467713e
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-04-22 13:45:13 +0400

    style : styled EmptyCharts.jsx component

--- frontend/src/components/analytics/Analytics.jsx
+++ frontend/src/components/analytics/Analytics.jsx
@@ -1,11 +1,14 @@
+import EmptyCharts from "../../empty/EmptyCharts"
 import Charts from "./Charts"
 import Stats from "./Stats"
 
 const Analytics = ({ setChartsDate, chartsDate, chartsData }) => {
     return(
         <div className="analytics-container my-5">
-            <div className="analytics-end"><Stats chartsData={chartsData}/></div>
-            <div className="analytics-end"><Charts setChartsDate={setChartsDate} chartsDate={chartsDate} chartsData={chartsData}/></div>
+            <div className="analytics-start"><Stats chartsData={chartsData}/></div>
+            <div className="analytics-end">
+                {chartsData.length !== 0 ? <EmptyCharts /> : <Charts setChartsDate={setChartsDate} chartsDate={chartsDate} chartsData={chartsData}/>}
+            </div>
         </div>
     )
 }
--- frontend/src/empty/EmptyCharts.jsx
+++ frontend/src/empty/EmptyCharts.jsx
@@ -1,7 +1,26 @@
+import { useState } from "react";
+
 const EmptyCharts = () => {
+
+    const [chartsDate , setChartsDate] = useState("Month")
+
     return(
         <div className="empty-charts-container">
-            askmdlasm
+            <div className="charts-header">
+                <div className="charts-header-start">
+                    <h4><i class="fa-solid fa-chart-column"></i> Performance Analytics</h4>
+                </div>
+                <div className="charts-header-end">
+                    <button className={`btn border-0 chartsDate ${chartsDate == 'Week' ? 'active' : ""}`} onClick={() => setChartsDate(prev => prev !== 'Week' ? "Week" : prev)}>7 Days</button>
+                    <button className={`btn border-0 chartsDate ${chartsDate == 'Month' ? 'active' : ""}`} onClick={() => setChartsDate(prev => prev !== "Month" ? "Month" : prev)}>30 Days</button>
+                </div>
+            </div>
+
+            <div className="empty-charts-main">
+                <i class="fa-solid fa-chart-simple"></i>
+                <h4 className="fw-bold">No sales data found.</h4>
+                <h6 className="fw-light">Wait for the first product order to begin tracking performance and revenue growth.</h6>
+            </div>
         </div>
     );
 };
