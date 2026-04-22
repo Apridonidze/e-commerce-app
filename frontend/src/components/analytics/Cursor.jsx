f1185d2abf1ed4216bc1a914fad4e0f732a77cc2
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-04-22 14:55:32 +0400

    doc + cleanup : documentated and cleanued up ToolTip.jsx and Cursor.jsx components

--- frontend/src/components/analytics/Cursor.jsx
+++ frontend/src/components/analytics/Cursor.jsx
@@ -1,7 +1,7 @@
-const Cursor = ({x, y, width , height}) => {
+const Cursor = ({x, y, width , height}) => { //reciving props from parent component (ChartDashboard.jsx)
     return(
         <rect x={x} y={y} width={width} height={height} rx={5} ry={5} fill={'rgba(0, 0, 0, 0.25)'}/>
-    )
-}
+    );
+};
 
-export default Cursor;
+export default Cursor; //exporting component
--- frontend/src/components/analytics/ToolTip.jsx
+++ frontend/src/components/analytics/ToolTip.jsx
@@ -1,16 +1,16 @@
-const ToolTip = ({ active, payload, label }) => {
+const ToolTip = ({ active, payload }) => { //reciving props from parent component (ChartDashboard.jsx)
 
-    if (!active || !payload?.length) return null;
+    if (!active || !payload?.length) return null; //returning null if recieved props are invalid/undefined
 
     const revenue = payload.find(p => p.dataKey === "revenue")?.value;
-    const sales = payload.find(p => p.dataKey === "sales")?.value;
+    const sales = payload.find(p => p.dataKey === "sales")?.value; //defining revenue and sales value
 
     return(
         <div className="tooltip-container p-2 fs-6">
             <div ><i className="fa-solid fa-money-bill-trend-up"></i> Revenue : <b>${revenue.toFixed(2)}</b></div>
             <div><i className="fa-solid fa-cart-arrow-down"></i> Sales : <b>{sales}</b></div>
         </div>
-    )
-}
+    );
+};
 
-export default ToolTip;
+export default ToolTip; //exporting component
