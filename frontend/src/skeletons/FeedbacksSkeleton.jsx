a08847c197f581c3396749f62f2fecd8c30288f8
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-04-23 17:58:49 +0400

    feat: developed FeedbacksSkeleton.jsx

--- frontend/src/skeletons/FeedbacksSkeleton.jsx
+++ frontend/src/skeletons/FeedbacksSkeleton.jsx
@@ -4,7 +4,17 @@ import "react-loading-skeleton/dist/skeleton.css"; //importing react loading ske
 const FeedbacksSkeleton = () => {
     return(
         <SkeletonTheme baseColor="#e9ecf5A1" highlightColor="#f5f7ffA1">
-            <div className="feedback-loading-skeleton">
+            <div className="reports-loading-skeleton mt-5" >
+                <Skeleton width={50} style={{marginRight : '1rem'}} inline height={30}/>
+                <Skeleton width={250} inline height={30}/>
+
+                <div className="row row-cols-md-3 row-cols-lg-5 row-cols-sm-12 row-cols-1 mt-2">
+                    <Skeleton width={'100%'} height={200}/>
+                    <Skeleton width={'100%'} height={200}/>
+                    <Skeleton width={'100%'} height={200}/>
+                    <Skeleton width={'100%'} height={200}/>
+                    <Skeleton width={'100%'} height={200}/>
+                </div>
 
             </div>
         </SkeletonTheme>
