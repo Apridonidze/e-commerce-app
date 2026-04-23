f504df2c841a029fe9f687a0fa4f7eafbb5e94c4
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-04-23 18:40:25 +0400

    cleanup : cleaned up EmptyOrders.jsx component

--- frontend/src/empty/EmptyOrders.jsx
+++ frontend/src/empty/EmptyOrders.jsx
@@ -1,10 +1,8 @@
-const EmptyOrders = ({ status }) => { //importing params from parent component (ManageOrders.jsx)
+const EmptyOrders = () => { //importing params from parent component (AdminDashboard.jsx)
     return(
         <div className="empty-orders-container">
-            <div className="empty-order-icon">
-                {status === "Pending" ? <i class="fa-solid fa-hourglass"></i> :  status === "OnWay" ? <i class="fa-solid fa-truck"></i> : <i class="fa-solid fa-list-check"></i>}
-            </div>
-            <h3>You don’t have any {status?.toLowerCase()} orders yet</h3>
+            <div className="empty-order-icon"><i class="fa-solid fa-list-check"></i></div>
+            <h3>You don’t have any  orders yet</h3>
             <h6>Once you place an order, it will appear here and move to delivered after completion.</h6>
         </div>
     );
