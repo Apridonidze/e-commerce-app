35cf934711eec383b76f946febc14e132de54d62
Giorgi Aphridonidze <giorgiapridonidze08@gmail.com>
2026-04-26 18:15:39 +0400

    style : styled NotFound.jsx component

--- frontend/src/routes/NotFound.jsx
+++ frontend/src/routes/NotFound.jsx
@@ -1,9 +1,16 @@
 import { Link } from "react-router-dom"
+import Footer from "../layout/Footer"
 const NotFound = () => {
     return(
-        <div className="not-found-container">
-            Page Not Found
-            <Link to='/'>Home</Link>
+        <div className="not-found-page-container">
+            <div className="not-found-start">
+                <div className="not-found-container mx-auto">
+                    <h1 className="fw-bold">Page Not Found</h1>
+                    <h6 className="small mb-5">The page you’re looking for doesn’t exist or may have been moved.</h6>
+                    <Link to='/'><button className="btn border-0 bg-0 fs-6 text-white"><i class="fa-solid fa-house text-white"></i> Back to Home</button></Link>
+                </div>
+            </div>
+            <Footer />
         </div>
     )
 }
